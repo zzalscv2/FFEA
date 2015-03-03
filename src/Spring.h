87ba1a12d4da7f700d422b70cb1abc021d695e42
Ben Hanson <py09bh@leeds.ac.uk>
2015-03-03 12:02:37 +0000

    Added Spring.h
    Former-commit-id: 998e785e9b1535fe1a0603ad36228a309b327923

---
+++ src/Spring.h
@@ -0,0 +1,44 @@
+#include "mat_vec_types.h"
+
+class Spring {
+
+	public:
+	
+		Spring() {
+			blob_index = new int[2];
+			conformation_index = new int[2];
+			node_index = new int[2];
+			k = 0.0;
+			l = 0.0;
+		}
+
+		~Spring() {
+			delete[] blob_index;
+			blob_index = NULL;
+			delete[] conformation_index;
+			conformation_index = NULL;
+			delete[] node_index;
+			node_index = NULL;
+			k = 0.0;
+			l = 0.0;
+		}
+
+		/* 
+		 *  Variables
+		 */
+
+		// Spring constant
+		scalar k;
+		
+		// Equilibrium length
+		scalar l;
+		
+		// Blobs connected to
+		int *blob_index;
+
+		// Conformations connected to
+		int *conformation_index;
+	
+		// Nodes connected to
+		int *node_index;
+};
