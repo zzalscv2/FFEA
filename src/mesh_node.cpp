ab391d3e2f0307ebc177c53ce659cce4d302684a
Albert Solernou <a.solernou@leeds.ac.uk>
2015-03-27 17:08:40 +0000

    nothing
    Former-commit-id: 274312e15f75be4b545606b4f8680b2495358790
    Former-commit-id: 76350ae356cc3c18cd1d78cf4a5c2cb8fcd38b19

--- src/mesh_node.cpp
+++ src/mesh_node.cpp
@@ -8,35 +8,35 @@
 /*
  * Structure for a mesh_node: the points FEM meshes are built from.
  */
-		mesh_node::mesh_node()
-		{
-			num_element_contributors = 0;
-			force_contributions = NULL;
-			pos.x = 0; pos.y = 0; pos.z = 0;
-			vel.x = 0; vel.y = 0; vel.z = 0;
-			phi = 0;
-			index = 0;
-			pos_0.x = 0; pos_0.y = 0; pos_0.z = 0;
-			stokes_radius = 0;
-			stokes_drag = 0;
-		}
+mesh_node::mesh_node()
+{
+	num_element_contributors = 0;
+	force_contributions = NULL;
+	pos.x = 0; pos.y = 0; pos.z = 0;
+	vel.x = 0; vel.y = 0; vel.z = 0;
+	phi = 0;
+	index = 0;
+	pos_0.x = 0; pos_0.y = 0; pos_0.z = 0;
+	stokes_radius = 0;
+	stokes_drag = 0;
+}
 
-		mesh_node::~mesh_node()
-		{
-			delete[] force_contributions;
-			pos.x = 0; pos.y = 0; pos.z = 0;
-			vel.x = 0; vel.y = 0; vel.z = 0;
-			phi = 0;
-			index = 0;
-			pos_0.x = 0; pos_0.y = 0; pos_0.z = 0;
-			num_element_contributors = 0;
-			stokes_radius = 0;
-			stokes_drag = 0;
-		}
+mesh_node::~mesh_node()
+{
+	delete[] force_contributions;
+	pos.x = 0; pos.y = 0; pos.z = 0;
+	vel.x = 0; vel.y = 0; vel.z = 0;
+	phi = 0;
+	index = 0;
+	pos_0.x = 0; pos_0.y = 0; pos_0.z = 0;
+	num_element_contributors = 0;
+	stokes_radius = 0;
+	stokes_drag = 0;
+}
 
-		void mesh_node::print()
-		{
-			printf("pos: %e %e %e\n", pos.x, pos.y, pos.z);
-			printf("vel: %e %e %e\n", vel.x, vel.y, vel.z);
-		}
+void mesh_node::print()
+{
+	printf("pos: %e %e %e\n", pos.x, pos.y, pos.z);
+	printf("vel: %e %e %e\n", vel.x, vel.y, vel.z);
+}
 
