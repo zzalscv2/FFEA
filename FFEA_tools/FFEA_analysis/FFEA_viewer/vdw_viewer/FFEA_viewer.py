c373a23dd1f72121ff16300b24cd089167463aac
Ben Hanson <py09bh@leeds.ac.uk>
2015-03-30 17:13:02 +0100

    Added help for FFEA_viewer
    Former-commit-id: 2a4e25f153b363f53e356285112dfe871e8b50ea

--- FFEA_tools/FFEA_analysis/FFEA_viewer/vdw_viewer/FFEA_viewer.py
+++ FFEA_tools/FFEA_analysis/FFEA_viewer/vdw_viewer/FFEA_viewer.py
@@ -16,31 +16,41 @@ import time
 import FFEA_viewer_control_window
 import FFEA_viewer_display_window
 
+def print_help():
+	print "Usage: python " + sys.argv[0] + " [options]"
+	print "Options:"
+	print "  -h/--help\t\t\t\tdisplay this help"
+	print "  -s/--script 'fname'\t\t\tscript fname to use"
+	print "  -f/--frames 'num_frames'\t\tnumber of frames to read"
+	print "  -e/--energy 'energy_thresh'\t\tenergy threshold (no idea what this does!)"
 
 file_to_load = None
 energy_thresh = 1e-6
 num_frames_to_read = float("inf")
 
 for i in range(1, len(sys.argv), 2):
-	if sys.argv[i] == "-script":
+	if sys.argv[i] == "-h" or sys.argv[i] == "--help":
+		print_help()
+		sys.exit()
+	elif sys.argv[i] == "-s" or sys.argv[i] == "--script":
 		try:
 			file_to_load = sys.argv[i + 1]
 		except IndexError:
 			print "Parameter not specified"
-			break
+			continue
 
-	elif sys.argv[i] == "-frames":
+	elif sys.argv[i] == "-f" or sys.argv[i] == "--frames":
 		try:
 			num_frames_to_read = int(sys.argv[i + 1])
 		except IndexError:
 			print "Parameter not specified"
-			break
-	elif sys.argv[i] == "-energy":
+			continue
+	elif sys.argv[i] == "-e" or sys.argv[i] == "--energy":
 		try:
 			energy_thresh = float(sys.argv[i + 1])
 		except IndexError:
 			print "Parameter not specified"
-			break
+			continue
 	else:
 		print "Unrecognised flag '" + sys.argv[i] + "'\n"
 		break
