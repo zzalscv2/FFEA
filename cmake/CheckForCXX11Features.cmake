6cfd5a886d6f68966229faf96efee3024afa4869
Albert Solernou <a.solernou@leeds.ac.uk>
2015-07-30 16:59:48 +0100

    CMake - Check more C++11 support features.
    Former-commit-id: fccec3aff01b36d66fc2306a92785cc9b3b074a7

--- cmake/CheckForCXX11Features.cmake
+++ cmake/CheckForCXX11Features.cmake
@@ -15,7 +15,7 @@ endif ()
 # CHECK 1 #### Check for stoi: 
 message(STATUS "Checking C++11 support for stoi")
 try_compile(RESULT_VAR 
-           "${CMAKE_CURRENT_BINARY_DIR}/cxx11_stoi}"
+           "${CMAKE_CURRENT_BINARY_DIR}/cxx11_stoi"
            "${PROJECT_SOURCE_DIR}/cmake/tests/stoi.cpp"
            COMPILE_DEFINITIONS "${CXX11_COMPILER_FLAGS} ${CMAKE_CXX_FLAGS}"
            OUTPUT_VARIABLE OUTPUT)
@@ -24,4 +24,20 @@ if (RESULT_VAR)
   message(STATUS "Checking C++11 support for stoi - Success")
 else ()
   message(FATAL_ERROR "Checking C++11 support for stoi - Failed")
+endif ()
+
+
+##############################
+# CHECK 2 #### Check for fstream open(string): 
+message(STATUS "Checking C++11 support for fstream-string")
+try_compile(RESULT_VAR 
+           "${CMAKE_CURRENT_BINARY_DIR}/cxx11_fstream-string"
+           "${PROJECT_SOURCE_DIR}/cmake/tests/fstream-string.cpp"
+           COMPILE_DEFINITIONS "${CXX11_COMPILER_FLAGS} ${CMAKE_CXX_FLAGS}"
+           OUTPUT_VARIABLE OUTPUT)
+
+if (RESULT_VAR)
+  message(STATUS "Checking C++11 support for fstream-string - Success")
+else ()
+  message(FATAL_ERROR "Checking C++11 support for fstream-string - Failed")
 endif ()
