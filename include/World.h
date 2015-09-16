4af6032c587a28a66ea4e7d5e2f71df2f6d8b3af
Albert Solernou <a.solernou@leeds.ac.uk>
2015-09-16 12:29:23 +0100

    +2 missing definitions, now FFEA compiles using the Intel Compilers!
    Former-commit-id: 4a082da41cf05faf94073dcb75699cae7a5ba3f8
    Former-commit-id: 19dcd13cdb878d2cb04e1978aae811a91bb4cc75

--- include/World.h
+++ include/World.h
@@ -80,6 +80,9 @@ public:
     /* */
     void get_system_dimensions(vector3 *dimenstion_vector);
 
+    int enm(int *blob_index, int num_modes);
+    int get_num_blobs();
+
 private:
 
     /** @brief How many Blobs populate this world */
