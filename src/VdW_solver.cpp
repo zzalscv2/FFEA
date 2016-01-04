d60b51302fbde4123366872f91d6618fefeba39b
Albert Solernou <a.solernou@leeds.ac.uk>
2016-01-04 16:58:54 +0000

    VdW_solver is inherited by Steric_solver; World treats vdw_solver as a pointer; Steric_solver overloads "do_interaction", in case of being loaded as vdw_solver.
    Former-commit-id: 883ae92aa954298340c3f87aa05cbaa43b3af593

--- src/VdW_solver.cpp
+++ src/VdW_solver.cpp
@@ -83,14 +83,14 @@ int VdW_solver::solve() {
             while (l_j != NULL) {
                 if (l_i->index != l_j->index) {
                     f_j = l_j->obj;
-                    if (f_i->daddy_blob != f_j->daddy_blob) {
+                    // if (f_i->daddy_blob != f_j->daddy_blob) {
                         //printf("(%d %d)\n", l_i->index, l_j->index);
                         //if((l_i->index == 1 && l_j->index == 5) || (l_i->index == 5 && l_j->index == 1))
                         f_i->set_vdw_bb_interaction_flag(true, f_j->daddy_blob->blob_index);
                         f_j->set_vdw_bb_interaction_flag(true, f_i->daddy_blob->blob_index);
                         do_interaction(f_i, f_j);
                         // do_volumeExclusion(f_i, f_j);
-                    }
+                    // }
                 }
                 l_j = l_j->next;
             }
@@ -123,83 +123,6 @@ int VdW_solver::solve_sticky_wall(scalar h) {
     return FFEA_OK;
 }
 
-/* do_volumeExclusion calculates the force (and not the energy, yet) of two tetrahedra */
-void VdW_solver::do_volumeExclusion(Face *f1, Face *f2){ 
-
-    // First check two things (either of which results in not having to calculate anything):
-    // Check that faces are facing each other, if not then they are not interacting
-    if (dot(&f1->normal, &f2->normal) > ffea_const::zero) {
-        return;
-    }
-
-    /* Robin suspects that this was leading to unstabilities...
-    // Check that faces are in front of each other
-    vector3 sep = {f2->centroid.x - f1->centroid.x, f2->centroid.y - f1->centroid.y, f2->centroid.z - f1->centroid.z};
-    if(dot(&sep, &f1->normal) < 0 && dot(&sep, &f2->normal) > 0) {
-        return;
-    }*/
-
-    //  Firstly, check that no nodes are shared:
-    if (f1->n[3] == f2->n[3]) {
-          return;
-    }
-    for (int i=0; i<4; i++) {
-        int in_i = f1->n[i]->index;
-        for (int j=0; j<4; j++) {
-           if (f2->n[j]->index == in_i){
-               return;
-           }
-        }
-    }
-
-    //  Then, check whether the tetrahedra intersect.
-    if (!f1->checkTetraIntersection(f2)) {
-      return; 
-    }
-
-    //   and get the direction of the force for f1:
-    /* TRIAL 2 */ 
-    arr3 force1, force2, n1_b; 
-    vec3Vec3SubsToArr3(f1->n[3]->pos, f2->n[3]->pos, force1);
-    arr3Normalise<scalar,arr3>(force1); // that is the direction of the force for f1 (backwards). 
-
-    /* TRIAL 1
-    arr3 force1, force2, n1_b; 
-    vec3ResizeToArr3(ffea_const::mOne, f1->normal, n1_b); 
-    vec3Arr3AddToArr3(f2->normal, n1_b, force1); 
-    arr3Normalise(force1); // that is the direction of the force for f1 (backwards). 
-    */ 
-
-    //////////////////////////////////////////////
-    /// One more check ////// One more check /////
-    arr3 inwards; 
-    vec3Vec3SubsToArr3(f1->n[3]->pos, f1->centroid, inwards); 
-    if (arr3arr3DotProduct<scalar,arr3>(inwards, force1) < ffea_const::zero) { 
-      return; 
-    } 
-    // end checking //////////////////////////////
-    
-    // scalar vol_f = 1
-    // Finally, get the intersection volume:
-    scalar vol = f1->getTetraIntersectionVolume(f2); 
-    /*if ((vol < 0) and (fabs(vol) > ffea_const::threeErr)) { 
-      cout << "The IntersectoMetre had a precision problem: " << vol << endl;
-      return; 
-    }*/ 
-
-
-    arr3Resize(vol, force1); 
-    arr3Resize2(ffea_const::mOne, force1, force2);
-    
-    for (int j = 0; j < 3; j++) {
-      f1->add_force_to_node(j, force1); 
-      f1->add_bb_vdw_force_to_record(force1, f1->daddy_blob->blob_index);
-      f2->add_force_to_node(j, force2); 
-      f2->add_bb_vdw_force_to_record(force2, f1->daddy_blob->blob_index);
-    } 
-
-
-}
 
 void VdW_solver::do_interaction(Face *f1, Face *f2) {
     // First check two things (either of which results in not having to calculate anything):
