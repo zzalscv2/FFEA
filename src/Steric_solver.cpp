ba9b0ff0562b2c582c9a7edeb853dda66f2b784c
Albert Solernou <a.solernou@leeds.ac.uk>
2016-01-20 17:17:11 +0000

    Steric: hardcoded proportionality factor.
    Former-commit-id: 7efc50bd360121dda3a06b04c77cd6eb4d83dc41
    Former-commit-id: a3e1d6b795959a4fb0617b71c8d2e303813e4f1a

--- src/Steric_solver.cpp
+++ src/Steric_solver.cpp
@@ -19,6 +19,7 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
     }
 
     //  Firstly, check that no nodes are shared:
+    /*
     if (f1->n[3] == f2->n[3]) {
           return;
     }
@@ -30,6 +31,7 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
            }
         }
     }
+    */
 
     //  Then, check whether the tetrahedra intersect.
     if (!f1->checkTetraIntersection(f2)) {
@@ -37,7 +39,7 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
     }
 
     //   and get the direction of the force for f1:
-    /* TRIAL 2 */ 
+    /* TRIAL 2 */
     arr3 force1, force2, n1_b; 
     vec3Vec3SubsToArr3(f1->n[3]->pos, f2->n[3]->pos, force1);
     arr3Normalise<scalar,arr3>(force1); // that is the direction of the force for f1 (backwards). 
@@ -46,16 +48,18 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
     arr3 force1, force2, n1_b; 
     vec3ResizeToArr3(ffea_const::mOne, f1->normal, n1_b); 
     vec3Arr3AddToArr3(f2->normal, n1_b, force1); 
-    arr3Normalise(force1); // that is the direction of the force for f1 (backwards). 
-    */ 
+    arr3Normalise<scalar,arr3>(force1); // that is the direction of the force for f1 (backwards).
+    */
 
     //////////////////////////////////////////////
     /// One more check ////// One more check /////
+    /*
     arr3 inwards; 
     vec3Vec3SubsToArr3(f1->n[3]->pos, f1->centroid, inwards); 
     if (arr3arr3DotProduct<scalar,arr3>(inwards, force1) < ffea_const::zero) { 
       return; 
     } 
+    */ 
     // end checking //////////////////////////////
     
     // scalar vol_f = 1
@@ -63,6 +67,9 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
     // scalar vol = f1->getTetraIntersectionVolume(f2); 
     geoscalar vol, area; 
     f1->getTetraIntersectionVolumeAndArea(f2,vol,area);
+    scalar f = 1e2;
+    area *= f;
+    vol *=f; 
 
     // Energy is proportional to the volume of interaction: 
     f1->add_bb_vdw_energy_to_record(vol, f2->daddy_blob->blob_index);
@@ -75,7 +82,7 @@ void Steric_solver::do_interaction(Face *f1, Face *f2){
     
     for (int j = 0; j < 3; j++) {
       f1->add_force_to_node(j, force1); 
-      f1->add_bb_vdw_force_to_record(force1, f1->daddy_blob->blob_index);
+      f1->add_bb_vdw_force_to_record(force1, f2->daddy_blob->blob_index);
       f2->add_force_to_node(j, force2); 
       f2->add_bb_vdw_force_to_record(force2, f1->daddy_blob->blob_index);
     } 
