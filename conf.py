9220704b5f1daf09cae0119fb0e1c854ea8086a6
Albert Solernou <a.solernou@leeds.ac.uk>
2016-03-10 15:15:55 +0000

    [DOCUMENTATION] testing readthedocs

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ import subprocess, os, re
 
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-read_the_docs_build = True
+# read_the_docs_build = True
 
 if read_the_docs_build:
 
@@ -15,6 +15,10 @@ if read_the_docs_build:
     for i in STA: 
       txt = re.sub("@CMAKE_CURRENT_SOURCE_DIR@",".",i)
       txt = re.sub("@PACKAGE_VERSION@","0.9",txt)
+      l = txt.split()
+      if len(l) > 1:
+        if l[0] == "OUTPUT_DIRECTORY":
+          txt = "OUTPUT_DIRECTORY		= .\n"
       sta.write(txt)
     sta.close()
     
