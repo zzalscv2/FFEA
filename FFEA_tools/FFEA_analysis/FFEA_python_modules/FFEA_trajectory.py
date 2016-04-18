8d40ed859480be516783bfd086fcbbb4ffc727f1
Ben Hanson <bhanson91@hotmail.co.uk>
2016-04-18 18:53:00 +0100

    Improved analysis tool for interface with pyPca Improved viewer Improved modules for the mapping procedure

--- FFEA_tools/FFEA_analysis/FFEA_python_modules/FFEA_trajectory.py
+++ FFEA_tools/FFEA_analysis/FFEA_python_modules/FFEA_trajectory.py
@@ -1,10 +1,11 @@
 from os import path
 import numpy as np
 import FFEA_frame, FFEA_pdb
+import sys
 
 class FFEA_trajectory:
 
-	def __init__(self, fname="", surf=None, load_all=1):
+	def __init__(self, fname="", surf=None, load_all=1, frame_rate = 1):
 
 		self.reset()
 
@@ -12,11 +13,11 @@ class FFEA_trajectory:
 		if fname == "":
 			return
 
-		if self.load(fname, load_all=load_all, surf=surf) == 1:
+		if self.load(fname, load_all=load_all, surf=surf, frame_rate = frame_rate) == 1:
 			print("\tLoading of '" + fname + "' failed. Returning empty object...")
 			return	
 		
-	def load(self, fname, surf=None, load_all=1):
+	def load(self, fname, surf=None, load_all=1, frame_rate = 1):
 
 		print("Loading FFEA trajectory file...")
 
@@ -36,15 +37,19 @@ class FFEA_trajectory:
 
 		# Then rest of trajectory.
 		if(load_all == 1):
+			all_frames = -1
 			while(True):
-			#while(self.num_frames < 100):
-				if(self.load_frame(surf=surf) != 0):
+				all_frames += 1
+				if all_frames % frame_rate != 0:
+					if self.skip_frame() == -1:
+						break
+				
+				elif(self.load_frame(surf=surf) != 0):
 					print("done! Successfully read " + str(self.num_frames) + " frame/s from '" + fname + "'.")
 					break
 
-	def load_from_pdb(self, pdb):
-
-			
+				if all_frames % 100 == 0:
+					print "Frames parsed = ", str(all_frames)
 
 	def load_header(self, fname):
 
@@ -146,8 +151,10 @@ class FFEA_trajectory:
 				
 			# Get a frame first (STATIC should just continue, single frame can be added prior or after)
 			if self.traj.readline().strip() == "DYNAMIC":
+				b[cindex].motion_state = "DYNAMIC"
 				frame = FFEA_frame.FFEA_frame()
 			else:
+				b[cindex].motion_state = "STATIC"
 				continue
 
 			# Read stuff
@@ -174,6 +181,32 @@ class FFEA_trajectory:
 		self.num_frames += 1
 		return 0
 
+	def skip_frame(self):
+
+		num_asterisks = 0
+		while(True):
+			line = self.traj.readline()
+			try:
+				if line[0] == "*":
+					num_asterisks += 1
+					if num_asterisks == 2:
+						break
+			except:
+				return -1
+
+	def build_from_pdb(self, pdb, scale = 1):
+
+		# Single blob single conf
+		self.set_header(1, [1], [[pdb.blob[0].num_atoms]])
+		print scale
+		for i in range(pdb.num_frames):
+			frame = FFEA_frame.FFEA_frame()
+			frame.pos = pdb.blob[0].frame[i].pos * scale
+			frame.set_step(i)
+			self.blob[0][0].num_nodes = len(frame.pos)
+			self.blob[0][0].frame.append(frame)
+			self.num_frames += 1
+
 	# Manually set a single frame
 	def set_single_frame(self, node, surf = None, step = 0):
 
@@ -199,7 +232,78 @@ class FFEA_trajectory:
 					self.blob[i][j].frame.append(None)
 		
 		self.num_frames = 1
+	
+	def write_to_file(self, fname, frames=None, frame_rate = 1):
+
+		# Get a file object
+		fout = open(fname, "w")
 		
+		# Write header info
+		self.write_header_to_file(fout)
+
+		# Write frames
+		if frames == None:
+			frames = [0,self.num_frames]
+
+		for i in range(frames[0], frames[1], frame_rate):
+			self.write_frame_to_file(fout, i)
+
+		fout.close()
+
+	def write_header_to_file(self, fout):
+
+		
+		fout.write("FFEA_trajectory_file\n\nInitialisation:\nNumber of Blobs %d\nNumber of Conformations" % (self.num_blobs))
+		for i in self.num_conformations:
+			fout.write(" %d" % (i))
+		fout.write("\n")
+		for i in range(self.num_blobs):
+			fout.write("Blob %d:" % (i))
+			for j in range(self.num_conformations[i]):
+				fout.write(" Conformation %d Nodes %d" % (j, self.num_nodes[i][j]))
+			fout.write("\n")
+		fout.write("\n*\n")
+
+	def write_frame_to_file(self, fout, index):
+
+		if index >= self.num_frames:
+			return
+
+		# Traj data
+		cur_conf = []
+		for b in self.blob:
+			bindex = self.blob.index(b)
+			for c in b:
+				cindex = b.index(c)
+				if c.motion_state == "STATIC":
+					fout.write("Blob %d, Conformation %d, step %d\nSTATIC\n" % (bindex, cindex, 0))
+					cur_conf.append(0)
+					break
+				else:
+					if c.frame[index] == None:
+						continue
+				
+					cur_conf.append(cindex)
+					fout.write("Blob %d, Conformation %d, step %d\n" % (bindex, cindex, c.frame[index].step))
+					fout.write(c.motion_state + "\n")
+					c.frame[index].write_to_traj(fout)
+
+		# Kinetic Data
+		fout.write("*\nConformation Changes:\n")
+		for b in self.blob:
+			bindex = self.blob.index(b)
+			try:
+				for c in b:
+					cindex = b.index(c)
+					if c.frame[index + 1] != None:
+						next_conf = cindex
+						break
+			except:
+				next_conf = cur_conf[bindex]
+
+			fout.write("Blob %d: Conformation %d -> Conformation %d\n" % (bindex, cur_conf[bindex], next_conf))
+		fout.write("*\n")		
+
 	def reset(self):
 
 		self.traj = None
@@ -259,6 +363,7 @@ class FFEA_traj_blob:
 
 	def reset(self):
 
+		self.motion_state = "DYNAMIC"
 		self.num_nodes = 0
 		self.num_subblobs = 0
 		self.frame = []
