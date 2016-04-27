8055ab132e7a366661db36f655dbf590ed04a93d
Ben Hanson <bhanson91@hotmail.co.uk>
2016-04-27 16:26:48 +0100

    Changed run_tests.sh to run correctly for all tests from the root directory
    Former-commit-id: aebbdaef3b16361d4ca2de227d0159eb823f29ba
    Former-commit-id: bbe28536e22011841eb8f795f14bcb7566ca1613

--- tests/tests_physics/run_tests.sh
+++ tests/tests_physics/run_tests.sh
@@ -8,65 +8,64 @@ echo " "
 echo "ffea: Usage..."
 echo " "
 sleep 3
-$ffeadir/ffea
+ffea
 
 echo " "
 echo "ffea: Simulation of Flat Cuboid..."
-echo "          num_elements = 465"
-echo "          num_nodes    = 1025"
+echo "          num_elements = 179"
+echo "          num_nodes    = 452"
 echo " "
 sleep 3
-$scriptdir/cuboid_flat/scripts/cuboid_flat.ffea
+ffea $scriptdir/cuboid_flat/cuboid_flat.ffea
 
 echo " "
 echo "ffea: Simulation of Multiple Flat Cuboids Interacting..."
-echo "          num_elements = 465"
-echo "          num_nodes    = 1025"
+echo "          num_elements = 179"
+echo "          num_nodes    = 1452"
 echo " "
 sleep 3
-$scriptdir/cuboid_flat/scripts/cuboid_flat_multiple.ffea
+ffea $scriptdir/cuboid_flat/cuboid_flat_multiple.ffea
 
-#echo " "
-#echo "ffea: running with mass..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea $scriptdir/sphere_63_120_mass/sphere_63_120_mass.ffea
+echo " "
+echo "ffea: running with mass..."
+echo " "
+sleep 3
+ffea $scriptdir/sphere_63_120_mass/sphere_63_120_mass.ffea
 
-#echo " "
-#echo "ffea: running with no mass..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea $scriptdir/sphere_63_120_nomass/sphere_63_120_nomass.ffea
+echo " "
+echo "ffea: running with no mass..."
+echo " "
+sleep 3
+ffea $scriptdir/sphere_63_120_nomass/sphere_63_120_nomass.ffea
 
-#echo " "
-#echo "ffea: running with multiple non-interacting blobs with no mass..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea $scriptdir/sphere_63_120_two/sphere_63_120_two.ffea
+echo " "
+echo "ffea: running with multiple non-interacting blobs with no mass..."
+echo " "
+sleep 3
+ffea $scriptdir/sphere_63_120_two/sphere_63_120_two.ffea
 
+echo " "
+echo "ffea: running with multiple interacting blobs with mass..."
+echo " "
+sleep 3
+ffea $scriptdir/sphere_63_120_two_vdw/sphere_63_120_two_vdw.ffea
 
-#echo " "
-#echo "ffea: running with multiple interacting blobs with mass..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea $scriptdir/sphere_63_120_two_vdw/sphere_63_120_two_vdw.ffea
+echo " "
+echo "ffea: running elastic network model of flat cuboid..."
+echo " "
+sleep 3
+ffea -m 1 $scriptdir/cuboid_flat/cuboid_flat_enm.ffea << EOF
+0
+q
+5
+EOF
 
-#echo " "
-#echo "ffea: running elastic network model of flat cuboid..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea -m 1 $scriptdir/cuboid_flat/scripts/cuboid_flat_enm.ffea << EOF
-#0
-#q
-#5
-#EOF
-
-#echo " "
-#echo "ffea: running elastic network model of flat cuboid..."
-#echo " "
-#sleep 3
-#$ffeadir/ffea -m 2 $scriptdir/cuboid_flat/scripts/cuboid_flat_dmm.ffea << EOF
-#0
-#q
-#5
-#EOF
+echo " "
+echo "ffea: running dynamic mode model of flat cuboid..."
+echo " "
+sleep 3
+ffea -m 2 $scriptdir/cuboid_flat/cuboid_flat_dmm.ffea << EOF
+0
+q
+5
+EOF
