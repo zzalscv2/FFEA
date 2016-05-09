zzalscv2/FFEA

commit 5f374c77aecb195c4eef2dbf3a924aeaf400cb1e

[PyMOL] plugin saves a lot of memory loading frames after reading them, and deleting the read data.

FFEA_tools/FFEA_analysis/pymol_plugin/__init__.py
@@ -536,15 +536,15 @@ class FFEA_viewer_control_window:
            pass
 
      # Reset initial camera (dependent upon structure size)
-     dims = self.get_system_dimensions()
-     self.dimensions = [dims[i][1] - dims[i][0] for i in range(3)]
-
-     if (self.dimensions[2] > self.dimensions[1]) and (self.dimensions[2] > self.dimensions[0]):
-         self.z = 2 * self.dimensions[2]
-     elif self.dimensions[0] > self.dimensions[1]:
-         self.z = self.dimensions[0] / (2 * np.tan(np.pi / 6.0))
-     else:
-        self.z = self.dimensions[1] / (2 * np.tan(np.pi / 6.0))
+     # dims = self.get_system_dimensions()
+     # self.dimensions = [dims[i][1] - dims[i][0] for i in range(3)]
+# 
+#      if (self.dimensions[2] > self.dimensions[1]) and (self.dimensions[2] > self.dimensions[0]):
+#          self.z = 2 * self.dimensions[2]
+#      elif self.dimensions[0] > self.dimensions[1]:
+#          self.z = self.dimensions[0] / (2 * np.tan(np.pi / 6.0))
+#      else:
+#         self.z = self.dimensions[1] / (2 * np.tan(np.pi / 6.0))
 
 
   def load_trajectory(self, trajectory_out_fname, ):
@@ -624,11 +624,6 @@ class FFEA_viewer_control_window:
           if self.num_frames >= self.num_frames_to_read:
               break
 
-          while self.pause_loading == True:
-              self.pausing = True
-              time.sleep(2)
-          self.pausing = False
-
           # skip asterisk
           line = traj.readline().rstrip()
           if line != "*":
@@ -671,6 +666,7 @@ class FFEA_viewer_control_window:
                   completed = 1
                   break
       		
+
           if completed == 0:
 
               # Get conformation data!
@@ -728,14 +724,26 @@ class FFEA_viewer_control_window:
                       self.num_frames += 1
                   else:
                       break
+
+              # plot the last frame and delete it to save memory:
+              self.blob_list[i][j].draw_frame(-1, self.display_flags)
+              self.blob_list[i][j].frames.pop() # we will only keep the frames
+                                             #  if somebody proves that they are useful
+
           else:
               break
 
       traj.close()
 
-      print "read ", self.num_frames, " frames"
+      # print "read ", self.num_frames, " frames"
+      print "finished loading frames"
 
-      self.draw_stuff()
+      # self.draw_stuff() # we are now drawing every frame after reading it. 
+      if self.num_frames > 1:
+        cmd.mset("1-"+str(self.num_frames))
+        if self.num_frames > 2:
+          cmd.mplay()
+
 
   def get_system_dimensions(self):
 
@@ -829,15 +837,9 @@ class FFEA_viewer_control_window:
 
   def draw_stuff(self):
 
-    # print "display flags: ", self.display_flags
     for f in range(self.num_frames):
       for i in range(self.num_blobs):
           for j in range(self.num_conformations[i]):
               self.blob_list[i][j].draw_frame(f, self.display_flags) ## PLUGIN OUT
-              # self.blob_list[i][j].draw_frame(self.frame, self.display_flags) ## PLUGIN OUT
-    if self.num_frames > 1:
-      cmd.mset("1-"+str(self.num_frames))
-      if self.num_frames > 2:
-        cmd.mplay()
 
 

FFEA_tools/FFEA_analysis/pymol_plugin/Blob.py
@@ -434,6 +434,7 @@ class Blob:
 					self.first_frame_J_inv.append(self.get_J_inv(n1, n2, n3, n4))
 				print "...Done."
 				self.calculated_first_frame_J_inv = True
+ 
 
 	def delete_all_frames(self):
 		self.num_frames = 0
@@ -678,7 +679,6 @@ class Blob:
 		return f.centroid_x, f.centroid_y, f.centroid_z
 
 	def draw_frame(self, i, display_flags):
-	# def __call__(self, i, display_flags):
 
 		if self.state == "STATIC":
 			i = 0
@@ -702,6 +702,10 @@ class Blob:
 				return
 		
                 obj = []
+                if i == -1:
+                  frameLabel = self.num_frames
+                else: 
+                  frameLabel = i
 
 		if display_flags['vdw_edit_mode'] == 1 and self.id_num == display_flags['selected_index']:
 			glBegin(GL_TRIANGLES)
@@ -930,7 +934,7 @@ class Blob:
                                 obj.extend( [ VERTEX, n3[0], n3[1], n3[2] ] )
 
                         obj.append(END)
-                        print "loading frame ", i, " for blob ", self.id_num
+                        print "loading frame ", frameLabel, " for blob ", self.id_num
                         # cmd.load_cgo(obj, "blob_" + str(self.id_num), i+1)
 
 		if display_flags['show_mesh_surf'] == 1:
@@ -1212,7 +1216,7 @@ class Blob:
 			# 		glRasterPos3f(nn[0], nn[1], nn[2])
 			# 		glutBitmapString(GLUT_BITMAP_HELVETICA_18, str(self.linear_nodes_only[n]));
                         cmd.set("cgo_line_radius",0.03)
-                        cmd.load_cgo(TXT,'Node numbers', i)
+                        cmd.load_cgo(TXT,'Node numbers', frameLabel)
 
 
 		if display_flags['show_pinned_nodes'] == 1:
@@ -1304,7 +1308,7 @@ class Blob:
 				print "---"
 
                 if len(obj) > 0:
-                  cmd.load_cgo(obj, "blob_" + str(self.id_num), i)
+                  cmd.load_cgo(obj, "blob_" + str(self.id_num), frameLabel)
 
 
 	def draw_pick_frame(self, i):

commit 2ba55f4d36f82d92ca91d4c1a4f97bb61c2513ae

[PyMOL] fixed bug introduced in 5f374c7. Only one blob was loaded.
Former-commit-id: c29f6c8b06f9865696fc7ae8f03c567b944dd76a
Former-commit-id: 2189cc9a90724ce795bebb07d99c3b5094b87e6d

FFEA_tools/FFEA_analysis/pymol_plugin/__init__.py
@@ -726,8 +726,10 @@ class FFEA_viewer_control_window:
                       break
 
               # plot the last frame and delete it to save memory:
-              self.blob_list[i][j].draw_frame(-1, self.display_flags)
-              self.blob_list[i][j].frames.pop() # we will only keep the frames
+              for i in range(self.num_blobs):
+                j = active_conf[i]
+                self.blob_list[i][j].draw_frame(-1, self.display_flags)
+                self.blob_list[i][j].frames.pop() # we will only keep the frames
                                              #  if somebody proves that they are useful
 
           else: