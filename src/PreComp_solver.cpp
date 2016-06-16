7acd9dd3ee75bc66833bc4009d9b1c156909c717
Albert Solernou <a.solernou@leeds.ac.uk>
2016-06-16 15:57:42 +0100

    [PreComp] "cout"s taken out.

--- src/PreComp_solver.cpp
+++ src/PreComp_solver.cpp
@@ -339,8 +339,8 @@ int PreComp_solver::solve() {
     compute_bead_positions();
 
     // 2 - Compute all the i-j forces:
-    scalar e_tot = 0.0; 
-    /*scalar f_tot = 0.0;*/
+    /*scalar e_tot = 0.0; 
+    scalar f_tot = 0.0;*/
     for (int i=0; i<n_beads; i++){ 
       type_i = b_types[i]; 
       phi_i[1] = b_rel_pos[3*i];
@@ -360,8 +360,8 @@ int PreComp_solver::solve() {
         
  
         f_ij = get_F(d, type_i, b_types[j]); 
-        e_tot += get_U(d, type_i, b_types[j]);
-        /*f_tot += f_ij;*/
+        /*e_tot += get_U(d, type_i, b_types[j]);
+        f_tot += f_ij;*/
         /*cout << "i: " << i << " j: " << j << " type_i: " << type_i << " type_j: " << b_types[j]
                       << " i.pos: " << mesoDimensions::length*b_pos[3*i]*1e9 << ":" << mesoDimensions::length*b_pos[3*i+1]*1e9 << ":" << mesoDimensions::length*b_pos[3*i+2]*1e9
                       << " j.pos: " << mesoDimensions::length*b_pos[3*j]*1e9 << ":" << mesoDimensions::length*b_pos[3*j+1]*1e9 << ":" << mesoDimensions::length*b_pos[3*j+2]*1e9
@@ -389,7 +389,7 @@ int PreComp_solver::solve() {
         } 
       }
     }
-    cout << " total energy: " << e_tot*mesoDimensions::Energy/0.1660539040e-20 << endl;
+    // cout << " total energy: " << e_tot*mesoDimensions::Energy/0.1660539040e-20 << endl;
   
     return FFEA_OK;
 }
