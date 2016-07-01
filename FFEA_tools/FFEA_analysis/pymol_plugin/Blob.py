zzalscv2/FFEA

commit c2c4feb2b4dc5eefca5f4802e664498f6d0d2a25

Fixed bug: On start, movement into the simulation box now works appropriately. There is a minor accuracy error to be fixed at a later date

FFEA_tools/FFEA_analysis/pymol_plugin/Blob.py
@@ -604,8 +604,8 @@ class Blob:
 		if self.frames[i] == None:
 			return None, None, None
 
-		if i < 0:
-			i = 0
+		#if i < 0:
+		#	i = 0
 
 		elif i >= self.num_frames:
 			i = self.num_frames - 1
@@ -642,7 +642,7 @@ class Blob:
 		pinsphere = []
 
 		print "loading frame ", frameLabel, " for blob ", self.idnum
-
+		frameLabel += 1
 		#
 		#  Solid    (always doable)
 		#

FFEA_tools/FFEA_analysis/pymol_plugin/__init__.py
@@ -322,7 +322,7 @@ class FFEA_viewer_control_window:
 			if b.index(c) == 0:
 		
 				c.set_nodes_as_frame()
-				print c.frames[0].pos[0]
+
 				x, y, z = c.get_centroid(0)
 				world_centroid[0] += x * c.node.num_nodes
 				world_centroid[1] += y * c.node.num_nodes
@@ -347,9 +347,9 @@ class FFEA_viewer_control_window:
 			# Do we need to calculate the box? Double the rounded up size of the system
 			if p.es_N_x < 1 or p.es_N_y < 1 or p.es_N_z < 1:
 				dims = self.get_system_dimensions(0)
-				p.es_N_x = int(dims[0] * (p.kappa / (p.es_h * self.global_scale)))
-				p.es_N_y = int(dims[1] * (p.kappa / (p.es_h * self.global_scale)))
-				p.es_N_z = int(dims[2] * (p.kappa / (p.es_h * self.global_scale)))
+				p.es_N_x = int(np.ceil(dims[0] * (p.kappa / (p.es_h * self.global_scale))))
+				p.es_N_y = int(np.ceil(dims[1] * (p.kappa / (p.es_h * self.global_scale))))
+				p.es_N_z = int(np.ceil(dims[2] * (p.kappa / (p.es_h * self.global_scale))))
 
 			self.box = True
 			self.box_x = 2 * (1.0 / p.kappa) * p.es_h * p.es_N_x
@@ -376,7 +376,6 @@ class FFEA_viewer_control_window:
 	shift[1] = self.box_y / 2.0 - world_centroid[1]
 	shift[2] = self.box_z / 2.0 - world_centroid[2]
 
-    
 	# Shift all blobs if necessary
 	for b in self.blob_list:
 		if p.calc_vdw == 1 and p.move_into_box == 1:
@@ -444,7 +443,6 @@ class FFEA_viewer_control_window:
 			# Delete frames from memory
 			traj.delete_frame()
 			self.remove_frame_from_blobs()
-
 		else:
 			break
 	return
@@ -616,6 +614,22 @@ class FFEA_viewer_control_window:
 	self.projection = "perspective"
 
 
+  def get_system_centroid(self, frameIndex = -1):
+
+	cent = np.array([0.0,0.0,0.0])
+	total_num_nodes = 0
+	for b in self.blob_list:
+		for c in b:
+			if b.index(c) == 0:
+
+				x, y, z = c.get_centroid(frameIndex)
+				cent[0] += x * c.node.num_nodes
+				cent[1] += y * c.node.num_nodes
+				cent[2] += z * c.node.num_nodes
+				total_num_nodes += c.node.num_nodes
+	cent *= 1.0 / total_num_nodes
+	return cent
+
   def draw_frame(self, index):
 
 	# Blobs should only ever have at most 2 frames on them, the initial one and the currently loaded one. So...
@@ -630,25 +644,11 @@ class FFEA_viewer_control_window:
 	if self.display_flags['show_box'] != 0 and self.box == True:
 		self.draw_box(frame_real_index)
 
+	print self.get_system_centroid()
+
 	for i in range(self.script.params.num_blobs):
 		for j in range(self.script.params.num_conformations[i]):
 			self.blob_list[i][j].draw_frame(frame_stored_index, frame_real_index, self.display_flags)
-  	
-  def draw_stuff(self):
-
-    # World first
-    if self.display_flags['show_box'] == 1:
-	self.draw_box()
-
-    for f in range(self.num_frames):
-      for i in range(self.script.params.num_blobs):
-          for j in range(self.script.params.num_conformations[i]):
-              self.blob_list[i][j].draw_frame(f, self.display_flags) ## PLUGIN OUT
-
-
-
-      if self.springs != None and self.display_flags['show_springs'] == 1:
-         self.draw_springs()
 
   def draw_box(self, f):
 	
@@ -740,7 +740,7 @@ class FFEA_viewer_control_window:
 					
 
 	obj.append(END)
-	cmd.load_cgo(obj, self.display_flags['system_name'] +"_Simulation_Box", f)
+	cmd.load_cgo(obj, self.display_flags['system_name'] +"_Simulation_Box", f + 1)
 
   def draw_springs(self):
 

src/World.cpp
@@ -241,7 +241,7 @@ int World::init(string FFEA_script_filename, int frames_to_delete, int mode) {
 		box_dim.x = params.es_h * (1.0 / params.kappa) * params.es_N_x;
 	        box_dim.y = params.es_h * (1.0 / params.kappa) * params.es_N_y;
 	        box_dim.z = params.es_h * (1.0 / params.kappa) * params.es_N_z;
-		
+
 		shift.x = box_dim.x / 2.0 - world_centroid.x;
 		shift.y = box_dim.y / 2.0 - world_centroid.y;
 		shift.z = box_dim.z / 2.0 - world_centroid.z;
@@ -253,6 +253,7 @@ int World::init(string FFEA_script_filename, int frames_to_delete, int mode) {
 			}
 		}
 	}
+
 	// Create measurement files
 	measurement_out = new FILE *[params.num_blobs + 1];
 
@@ -472,37 +473,6 @@ int World::init(string FFEA_script_filename, int frames_to_delete, int mode) {
 		}
 
 	}
-	 /*   // Initialise the Van der Waals solver
-	    if(params.calc_vdw == 1 || params.calc_es == 1) {
-		vector3 world_centroid, shift;
-	        get_system_centroid(&world_centroid);
-		if(params.es_N_x < 1 || params.es_N_y < 1 || params.es_N_z < 1) {
-			vector3 dimension_vector;
-			get_system_dimensions(&dimension_vector);
-			
-			// Calculate decent box size
-			params.es_N_x = 2 * (int)ceil(dimension_vector.x * (params.kappa / params.es_h));
-			params.es_N_y = 2 * (int)ceil(dimension_vector.y * (params.kappa / params.es_h));
-			params.es_N_z = 2 * (int)ceil(dimension_vector.z * (params.kappa / params.es_h));
-		}
-
-		// Move to box centre (if it is a new simulation! Otherwise trajectory will already have taken care of the move)
-		box_dim.x = params.es_h * (1.0 / params.kappa) * params.es_N_x;
-	        box_dim.y = params.es_h * (1.0 / params.kappa) * params.es_N_y;
-	        box_dim.z = params.es_h * (1.0 / params.kappa) * params.es_N_z;
-		
-		shift.x = box_dim.x / 2.0 - world_centroid.x;
-		shift.y = box_dim.y / 2.0 - world_centroid.y;
-		shift.z = box_dim.z / 2.0 - world_centroid.z;
-		if(params.move_into_box == 1 && params.restart == 0) {
-			for (i = 0; i < params.num_blobs; i++) {
-				//active_blob_array[i]->get_centroid(&world_centroid);
-				active_blob_array[i]->move(shift.x, shift.y, shift.z);
-				active_blob_array[i]->calc_all_centroids();
-			}
-		}
-	    }*/
-	// vector3 world_centroid;
 
 	    box_dim.x = params.es_h * (1.0 / params.kappa) * params.es_N_x;
 	    box_dim.y = params.es_h * (1.0 / params.kappa) * params.es_N_y;
@@ -2933,6 +2903,11 @@ void World::print_trajectory_and_measurement_files(int step, scalar wtime) {
 
     vector3 system_CoM;
     get_system_CoM(&system_CoM);
+
+    vector3 system_centroid;
+    get_system_centroid(&system_centroid);
+    cout << system_centroid.x*mesoDimensions::length*1e10 << " " << system_centroid.y*mesoDimensions::length*1e10 << " " << system_centroid.z*mesoDimensions::length*1e10 << endl;
+    //exit(0);
 
     // Write traj and meas data
     if (measurement_out[params.num_blobs] != NULL) {

commit 111bf3648ca47c8a93b5f3c6142151557e76b1b6

[PyMOL] bug fixed; indices not shown after bug introduced in c2c4feb2.
Former-commit-id: efa9a7f42cae5a0bc10e5d90df159e565e32b1b1
Former-commit-id: 23a33280a3f3af5fa46ac164766a3fc920a386fe

FFEA_tools/FFEA_analysis/pymol_plugin/Blob.py
@@ -761,7 +761,7 @@ class Blob:
 		if display_flags['show_numbers'] != 0:
 
 			# Only first frame
-			if frameLabel == 0:
+			if frameLabel == 1:
 					
 				axes = np.array([[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]])
 				scale = 10.0		# Maybe change me in the future to some clever function to do with the global scale? Or get rid of global scale...