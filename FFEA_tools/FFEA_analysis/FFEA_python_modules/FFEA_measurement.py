dd766ddf691358ed01f1f042534c0adc53bb8aa9
Ben Hanson <bhanson91@hotmail.co.uk>
2016-08-03 14:22:06 +0100

    Included a new script to test equilibration. Plots some pretty graphs, but only if you have matplotlib installed

--- FFEA_tools/FFEA_analysis/FFEA_python_modules/FFEA_measurement.py
+++ FFEA_tools/FFEA_analysis/FFEA_python_modules/FFEA_measurement.py
@@ -55,7 +55,7 @@ class FFEA_measurement:
 			line = fin.readline().strip()
 
 		# Build a dictionary of possible variables and a map to those
-		self.global_meas = {'Time': None, 'KineticEnergy': None, 'StrainEnergy': None, 'SpringEnergy': None, 'VdWEnergy': 'None', 'PreCompEnergy': None, 'Centroid.x': None, 'Centroid.y': None, 'Centroid.z': None, 'Centroid': None, 'RMSD': None}
+		self.global_meas = {'Time': None, 'KineticEnergy': None, 'StrainEnergy': None, 'SpringEnergy': None, 'VdWEnergy': None, 'PreCompEnergy': None, 'Centroid.x': None, 'Centroid.y': None, 'Centroid.z': None, 'Centroid': None, 'RMSD': None}
 		sline = fin.readline().strip().split()
 		measmap = ["" for i in range(len(sline))]		
 		i = -1
@@ -82,7 +82,8 @@ class FFEA_measurement:
 		del self.global_meas["Centroid.z"]
 	
 		for key in self.global_meas:
-			self.global_meas[key] = np.array(self.global_meas[key])
+			if self.global_meas[key] != None:
+				self.global_meas[key] = np.array(self.global_meas[key])
 
 	def load_detailed(self, fname):
 
@@ -171,11 +172,13 @@ class FFEA_measurement:
 			del self.blob_meas[i]["Centroid.z"]
 
 			for key in self.blob_meas[i]:
-				self.blob_meas[i][key] = np.array(self.blob_meas[i][key])
+				if self.blob_meas[i][key] != None:
+					self.blob_meas[i][key] = np.array(self.blob_meas[i][key])
 
 			for j in range(i, self.num_blobs):
 				for key in self.interblob_meas[i][j]:
-					self.interblob_meas[i][j][key] = np.array(self.interblob_meas[i][j][key])
+					if self.interblob_meas[i][j][key] != None:
+						self.interblob_meas[i][j][key] = np.array(self.interblob_meas[i][j][key])
 				self.interblob_meas[j][i] = self.interblob_meas[i][j]
 
 
@@ -185,180 +188,3 @@ class FFEA_measurement:
 		self.global_meas = None
 		self.blob_meas = []
 		self.interblob_meas = []
-				
-
-'''
-class FFEA_measurement_blob:
-
-	def __init__(self, fname = "", frame_rate=1, num_frames_to_read=1000000):
-	
-		self.reset()
-		if fname == "":
-			return
-
-		try:
-			self.load(fname, frame_rate=frame_rate, num_frames_to_read=num_frames_to_read)
-		except:
-			return
-	
-	def load(self, fname, frame_rate=1, num_frames_to_read=1000000):
-
-		# Test file exists
-		if not os.path.exists(fname):
-			print("\tFile '" + fname + "' not found.")
-			return
-
-		# Read (dictionary and measurement keys should be made to line up in the future, or make so that we can have arbitrary extra measurements after the core ones isn't needed)
-		try:
-			with open(fname, "r") as fin:
-
-				# Get to start
-				while(fin.readline()[0]) != "#":
-					pass
-
-				# Then, assign stuff
-				frame = -1
-				line = fin.readline()
-				while(line != ""):
-					
-					frame += 1
-
-					# Should we end?
-					if frame > num_frames_to_read:
-						break
-
-					# Should we skip?
-					if frame % frame_rate != 0:
-						line = fin.readline()
-						continue
-
-					sline = line.split()
-					try:
-						self.params['Step'].append(int(sline[0]))
-						self.params['Kinetic'].append(float(sline[1]))
-						self.params['Elastic'].append(float(sline[2]))
-						self.params['Centre'].append([float(sline[3]), float(sline[4]), float(sline[5])])
-						self.params['AMomentum'].append([float(sline[6]), float(sline[7]), float(sline[8])])
-						self.params['RMSD'].append(float(sline[9]))
-				
-						line = fin.readline()
-					except:
-						line = fin.readline()
-						continue
-		
-				# Make numpy for plotting and analysing and stuff
-				for key in self.params:
-					self.params[key] = np.array(self.params[key])
-		except:
-			print("\tUnable to load FFEA_measurement from " + fname + ". Returning empty object...")
-
-
-	def plot(self, key, **opts):
-		
-		if self.params.get(key) == None:
-
-			print("Error. '" + key + "' is not a recognised variable. Cannot be plotted.")
-			return
-		
-		# Send to the appropriate function
-		if key == "Elastic" or key == "Kinetic":
-			self.plot_energy(opts, energy=key)
-		
-		else:
-			print key + " is currently unsupported. Sorry. If you're a dev, program this in!"
-
-		
-	
-	def plot_energy(self, opts, energy=None):
-		
-		print("Currently unavailable")
-		return
-		if plt == False: 
-			print("plot_energy unavailable: matplotlib could not be imported")
-
-		"""
-		if energy == None:
-			print "Error. 'energy' should be 'Elastic' or 'Kinetic'"
-			return		
-		
-		# Default graphing options
-		plotType = {"hist": False, "logx": False, "logy": False, "ravg": False}
-		ylabel = energy + " Energy (J)"
-		y = self.params[energy]
-		print y
-		savefig = [False, None]
-
-		xlabel = "Step"
-		x = self.params['Step']
-		print x
-		# Get opts (this is super lazy, and will make the later options take priority rather than raise an error. But! It makes them mutually exclusive)
-		for o in opts:
-			
-			for t in plotType:
-				if t == o:
-					plotType[t] = True
-				else:
-					plotType[t] = False
-
-		
-		for o, val in opts.items():
-			if o.lower() == "kt" or o.lower() == "kbt":
-				ylabel = ylabel[0:-4] + "(kT)"
-				y /= float(val)
-			
-			elif o.lower() == "dt" or o.lower() == "step":
-				xlabel = "Time (ns)"
-				x *= float(val)
-				x /= 1e-9
-
-			elif o == "save" or o == "savefig":
-				savefig[0] = "True"
-				savefig[1] = val
-
-		# Do stuff
-		if plotType['ravg'] == True:
-			
-			avg = []
-			for i in range(len(y)):
-				avg.append(np.mean(y[0:i]))
-
-			plt.plot(x, avg)
-			plt.title(energy + " Energy Running Time Average")
-
-		elif plotType['logx'] == True:
-			
-			plt.semilogx(x, y)
-			plt.title(energy + " Energy Trace (log x)")
-			xlabel = "log " + xlabel[0:-4]
-
-		elif plotType['logy'] == True:
-			
-			plt.semilogx(x, y)
-			plt.title(energy + " Energy Trace (log y)")
-			ylabel = "log " + ylabel[0:-4]
-
-		elif plotType['hist'] == True:
-			
-			pass
-		else:
-
-			# Standard time trace
-			plt.plot(x,y)
-			plt.title(energy + " Energy Trace")
-
-		ax = plt.gca()
-		ax.set_ylim(bottom=0, top = max(y) * 1.5)
-		plt.xlabel(xlabel)
-		plt.ylabel(ylabel)
-	
-		if savefig[0] == True:
-			plt.savefig(savefig[1])
-
-		plt.show()
-		"""
-
-	def reset(self):
-
-		self.params = {'Step': [], 'Kinetic': [], 'Elastic': [], 'Centre': [], 'AMomentum': [], 'RMSD': []}
-
-'''
