70059cd9712957ebe4c3f0e6f402840fd71a4230
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-08-10 15:58:51 +0100

    More docs changes, prep for packaging
    Former-commit-id: 16b8c29d81cbb10811891a3cb5680b0c98749008

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ffeatools',
+      version='0.9',
+      description='FFEA file generation and analysis tools',
+      url='http://ffea.bitbucket.com',
+      author='FFEA Team',
+      author_email='???',
+      license='???',
+      packages=['ffeatools'],
+      zip_safe=False)
