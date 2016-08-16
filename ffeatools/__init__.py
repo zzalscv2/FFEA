f992e8f2575eb5034627334d9fc8148b5163a17f
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-08-16 12:50:45 +0100

    Wrapper
    Former-commit-id: d7b3cc14a6d7212fd0074c7cea9dc0b027194cf8

--- ffeatools/__init__.py
+++ ffeatools/__init__.py
@@ -3,11 +3,9 @@ import enthusiasm as _enthusiasm
 import os as _os
 import sys as _sys
 
-#add the modules folder (temporary hack until I can refactor)
-
+#Many of these scripts expect that the modules folder is on the system or python path.
 _sys.path.append(_os.path.join(_os.path.dirname(_enthusiasm.__file__), 'modules'))
     
-#from .FFEA_initialise import convert_pdb_to_emdb_map.py # need to make python wrapper first
-#from .FFEA_initialise imp
-    
 from . import modules
+from . import FFEA_initialise
+from . import FFEA_analysis
