18cb826683acb4eda17bc91f87850a44de91a07c
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-08-16 12:50:45 +0100

    Wrapper

--- ffeatools/FFEA_initialise/__init__.py
+++ ffeatools/FFEA_initialise/__init__.py
@@ -5,4 +5,16 @@ Created on Wed Aug 10 16:33:47 2016
 @author: py12rw
 """
 
-from .FFEA_python_modules import *
+from .EM_density_map_tools import convert_pdb_to_emdb_map # c
+from .FFEA_convert_from_volume import *
+#from .FFEA_cpp include *
+from .FFEA_vdw_tools import FFEA_get_vdw_area
+from .FFEA_vdw_tools import FFEA_make_bsites_vdwactive
+from .FFEA_volume_tools import convert_tet_to_net
+from .FFEA_volume_tools import cull_small_interior_elements
+from .FFEA_volume_tools import make_cuboid_mesh # c
+from .Surface_tools import surface_convert_from_EM_density_map # c
+from .Surface_tools import surface_coarse_grainer # c
+# check in document what comes after
+# api reference and python docs
+# wrappers if possible
