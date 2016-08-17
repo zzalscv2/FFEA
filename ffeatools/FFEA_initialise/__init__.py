8749dbf62265e10f1e0eeaa3eacdc41a21c9c8dc
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-08-17 12:11:11 +0100

    Finishing packaging, wrapping etc

--- ffeatools/FFEA_initialise/__init__.py
+++ ffeatools/FFEA_initialise/__init__.py
@@ -8,13 +8,12 @@ Created on Wed Aug 10 16:33:47 2016
 from .EM_density_map_tools import convert_pdb_to_emdb_map # c
 from .FFEA_convert_from_volume import *
 #from .FFEA_cpp include *
-from .FFEA_vdw_tools import FFEA_get_vdw_area
-from .FFEA_vdw_tools import FFEA_make_bsites_vdwactive
-from .FFEA_volume_tools import convert_tet_to_net
-from .FFEA_volume_tools import cull_small_interior_elements
+#from .FFEA_vdw_tools import FFEA_get_vdw_area # cli tool only
+#from .FFEA_vdw_tools import FFEA_make_bsites_vdwactive #cli tool only
+#from .FFEA_volume_tools import convert_tet_to_net # cli tool only
+#from .FFEA_volume_tools import cull_small_interior_elements # cli tool only
 from .FFEA_volume_tools import make_cuboid_mesh # c
-from .Surface_tools import surface_convert_from_EM_density_map # c
-from .Surface_tools import surface_coarse_grainer # c
+from .Surface_tools import * # c - EM density map and coarse grainer
 # check in document what comes after
 # api reference and python docs
 # wrappers if possible
