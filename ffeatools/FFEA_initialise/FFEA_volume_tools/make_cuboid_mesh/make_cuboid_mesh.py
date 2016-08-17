05b9086a997d8d23552c8cc829ef120fcf854860
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-08-17 12:11:11 +0100

    Finishing packaging, wrapping etc

--- ffeatools/FFEA_initialise/FFEA_volume_tools/make_cuboid_mesh/make_cuboid_mesh.py
+++ ffeatools/FFEA_initialise/FFEA_volume_tools/make_cuboid_mesh/make_cuboid_mesh.py
@@ -1,6 +1,6 @@
 import wrap
 
 def make_cuboid_mesh(*argv):
-    argv = wrap.santitize_tuple(argv)
+    argv = wrap.sanitize_tuple(argv)
     wrap.wrap_process("./make_cuboid_mesh", argv)
     return
