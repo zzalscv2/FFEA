fd08f99aa5f23b52155df119c032041df932f8d0
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-09-08 16:45:03 +0100

    Package structure housekeeping

--- ffeatools/__init__.py
+++ ffeatools/__init__.py
@@ -6,6 +6,10 @@ import sys as _sys
 #Many of these scripts expect that the modules folder is on the system or python path.
 _sys.path.append(_os.path.join(_os.path.dirname(_enthusiasm.__file__), 'modules'))
     
-from . import modules
-from . import FFEA_initialise
-from . import FFEA_analysis
+#from . import modules
+#from . import FFEA_initialise
+#from . import FFEA_analysis
+
+from .modules import __init__
+from .FFEA_initialise import __init__
+from .FFEA_analysis import __init__
