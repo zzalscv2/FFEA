21412c9db41c878267039bb564bca54acf2de83f
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-09-08 16:45:03 +0100

    Package structure housekeeping

---
+++ ffeatools_build/CMakeFiles/emdb_map_to_ffea.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/emdb_map_to_ffea.dir/emdb_map_to_ffea.o"
+  "emdb_map_to_ffea.pdb"
+  "emdb_map_to_ffea"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/emdb_map_to_ffea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
