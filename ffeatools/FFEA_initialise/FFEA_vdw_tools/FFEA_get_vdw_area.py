2cdf24f2634126cc6f0c51f0a9f16a25bfbbfd8c
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-09-19 15:42:16 +0100

    Finished packagizing ffeatools

--- ffeatools/FFEA_initialise/FFEA_vdw_tools/FFEA_get_vdw_area.py
+++ ffeatools/FFEA_initialise/FFEA_vdw_tools/FFEA_get_vdw_area.py
@@ -2,20 +2,32 @@ import sys
 import FFEA_surface, FFEA_node
 import FFEA_vdw
 
-if len(sys.argv) != 4:
-	sys.exit("Usage: python get_vdw_area.py [INPUT .vdw file] [INPUT .surf file] [INPUT .node file]")
+import argparse as _argparse
 
-# Get args
-vdwfname = sys.argv[1]
-surffname = sys.argv[2]
-nodefname = sys.argv[3]
+parser = _argparse.ArgumentParser(description="Get VDW Area")
+parser.add_argument("vdwfname", action="store", help="Input .vdw file")
+parser.add_argument("surffname", action="store", help="Input .surf file")
+parser.add_argument("nodefname", action="store", help="Input .node file")
 
-# Open files
-vdw = FFEA_vdw.FFEA_vdw(vdwfname)
-surf = FFEA_surface.FFEA_surface(surffname)
-node = FFEA_node.FFEA_node(nodefname)
+def FFEA_get_vdw_area(vdwfname, surffname, nodefname, print_only=False):
+    # Open files
+    vdw = FFEA_vdw.FFEA_vdw(vdwfname)
+    surf = FFEA_surface.FFEA_surface(surffname)
+    node = FFEA_node.FFEA_node(nodefname)
+    
+    areas = vdw.calc_active_areas(surf, node)
+    VDW_index = []
+    for i in range(-1,6,1):
+        if print_only:
+            print "VdW index " + str(i) + ": Area = " + str(areas[i + 1]) 
+        else:
+            VDW_index.append([i, areas[i+1]])
+    if print_only:
+        return
+    return VDW_index
+            
 
-areas = vdw.calc_active_areas(surf, node)
-for i in range(-1,6,1):
-
-	print "VdW index " + str(i) + ": Area = " + str(areas[i + 1]) 
+if sys.stdin.isatty():
+    args = parser.parse_args()
+    # Get args and build objects
+    FFEA_get_vdw_area(args.vdwfname, args.surffname, args.nodefname, print_only=True)
