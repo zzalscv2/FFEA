3d4d5666464ea9520ff00997543aac146d527ae6
Robert Welch [py12rw] <py12rw@phy-lin2121.leeds.ac.uk>
2016-09-23 16:23:26 +0100

    FFEA_meta added (still slightly untidy)
    Former-commit-id: 893d4dd35dd4f1a4f1043876649d1894016e8379
    Former-commit-id: 205cbd72b2555b71b055bbaf4b6906efd8e51dc8

--- ffeatools/FFEA_initialise/EM_density_map_tools/convert_pdb_to_emdb_map.py
+++ ffeatools/FFEA_initialise/EM_density_map_tools/convert_pdb_to_emdb_map.py
@@ -6,6 +6,7 @@ Created on Tue Aug 16 09:29:39 2016
 """
 
 import wrap
+import __builtin__
 
 def convert_pdb_to_emdb_map(*argv):
     """
@@ -16,6 +17,23 @@ def convert_pdb_to_emdb_map(*argv):
     convert_pdb_to_emdb_map(/path/to/input /path/to/output X_resolution Y_resolution Z_resolution atomic_radius)
     Where the atomic radius is given in angstroms.
     """
-    argv = wrap.santitize_tuple(argv)
+
+    # Log meta info    
+    
+    try:
+        convert_pdb_to_emdb_map = {}
+        convert_pdb_to_emdb_map["Input .PDB File"] = argv[0]
+        convert_pdb_to_emdb_map["Output .map filename"] = argv[1]
+        convert_pdb_to_emdb_map["X resolution"] = argv[2]
+        convert_pdb_to_emdb_map["Y resolution"] = argv[3]
+        convert_pdb_to_emdb_map["Z resolution"] = argv[4]
+        convert_pdb_to_emdb_map["Atomic Radius (angstroms)"] = argv[5]
+        __builtin__.meta_info.log["Convert DPB to EMDB map"] = convert_pdb_to_emdb_map
+    except:
+        print("Warning! failed to write log.")
+        pass
+
+    
+    argv = wrap.sanitize_tuple(argv)
     wrap.wrap_process("./convert_pdb_to_emdb_map", argv)
     return
