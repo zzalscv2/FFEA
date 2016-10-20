4ab6e7bc3d5ce2752122d14143daf6c3dc40251d
Albert Solernou <a.solernou@leeds.ac.uk>
2016-10-20 14:00:22 +0100

    [Performance] fdm file is not written if there is only a single blob in the system.

--- src/World.cpp
+++ src/World.cpp
@@ -157,6 +157,11 @@ int World::init(string FFEA_script_filename, int frames_to_delete, int mode, boo
 		return FFEA_ERROR;
 	}
 
+   if ((params.num_blobs) == 1) { 
+      writeDetailed = false;
+      printf("\n\tA single blob is simulated, and thus the detailed measurements are not needed\n"); 
+   } 
+
 	// Build kinetic maps if necessary (and rates and binding site matrix). These are at the World level in case global kinetic calculations are ever included
 	if(params.calc_kinetics == 1) {
 
