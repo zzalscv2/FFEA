4b81387457959e5c2623f9989da784c23bb2a2fc
Ben Hanson <bhanson91@hotmail.co.uk>
2016-11-17 11:19:36 +0000

    Made the map generator delete the correct files and also removed the sparse converter script (which is now build into the c++ tool)

--- ffeatools/FFEA_initialise/FFEA_mapping_tools/FFEA_generate_kinetic_maps.py
+++ ffeatools/FFEA_initialise/FFEA_mapping_tools/FFEA_generate_kinetic_maps.py
@@ -27,21 +27,22 @@ targettop = script.blob[bindex[1]].conformation[cindex[1]].topology
 basetotargetmap = infname.split(".")[0] + "_b" + str(bindex[0]) + "c" + str(cindex[0]) + "to" + "b" + str(bindex[1]) + "c" + str(cindex[1]) + ".map"
 targettobasemap = infname.split(".")[0] + "_b" + str(bindex[1]) + "c" + str(cindex[1]) + "to" + "b" + str(bindex[0]) + "c" + str(cindex[0]) + ".map"
 
-basetotargetsparsemap = basetotargetmap.split(".")[0] + "_sparse.map"
-targettobasesparsemap = targettobasemap.split(".")[0] + "_sparse.map"
+#basetotargetsparsemap = basetotargetmap.split(".")[0] + "_sparse.map"
+#targettobasesparsemap = targettobasemap.split(".")[0] + "_sparse.map"
 
 os.system(scriptdir + "/make_structure_map -i " + basenode + " -t " + basetop + " -o " + targetnode + " -m " + basetotargetmap)
 os.system(scriptdir + "/make_structure_map -i " + targetnode + " -t " + targettop + " -o " + basenode + " -m " + targettobasemap)
 
 # Make them sparse
-os.system("python " + scriptdir + "/FFEA_convert_kinetic_map_to_sparse.py " + basetotargetmap + " " + basetotargetsparsemap)
-os.system("python " + scriptdir + "/FFEA_convert_kinetic_map_to_sparse.py " + targettobasemap + " " + targettobasesparsemap)
+#os.system("python " + scriptdir + "/FFEA_convert_kinetic_map_to_sparse.py " + basetotargetmap + " " + basetotargetsparsemap)
+#os.system("python " + scriptdir + "/FFEA_convert_kinetic_map_to_sparse.py " + targettobasemap + " " + targettobasesparsemap)
 
 # Remove the unneeded stuff
 #os.system("rm " + basenode + " " + targetnode + " " + basetotargetmap + " " + targettobasemap)
-os.system("rm " + basetotargetmap + " " + targettobasemap)
+#os.system("rm " + basetotargetmap + " " + targettobasemap)
+os.system("rm " + basenode + " " + targetnode)
 
 # Finalise
 print("Maps created:")
-print("\t" + basetotargetsparsemap)
-print("\t" + targettobasesparsemap)
+print("\t" + basetotargetmap)
+print("\t" + targettobasemap)
