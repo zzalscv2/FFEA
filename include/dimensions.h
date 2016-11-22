6de36fd6ff07147b088f66a94777b4b0cd06d096
Albert Solernou <a.solernou@leeds.ac.uk>
2016-11-22 11:02:32 +0000

    [Dimensions] less rounding error, on derived units.
    Former-commit-id: 9499b6e66d16ad327376e65a550acedf875e5170
    Former-commit-id: f3611d0809dd43f1479e68c514a01ad86437d760

--- include/dimensions.h
+++ include/dimensions.h
@@ -9,12 +9,12 @@ namespace mesoDimensions {
    const scalar Energy = 4.141945559999999e-21; ///< Energy = KbT, T=300K
    const scalar mass = 1.994307387553024e-26; ///< mass = C atom mass
    const scalar charge = 1.602176565e-19;  ///< charge = electron charge
-   const scalar area = 2.89e-20; ///< area
-   const scalar volume = 4.913e-30; ///< volume
+   const scalar area = 2.89e-20; ///< area = l**2
+   const scalar volume = 4.913e-30; ///< volume = l**3
    const scalar force = 2.4364705882352941e-11; ///< force = E/l
-   const scalar time = 3.7302670416342907e-13; ///< time = sqrt(m*l/f)
-   const scalar pressure = 8.4306940769387329e8; ///< pressure
-   const scalar velocity = 4.5573144791671604e2; ///< velocity
+   const scalar time = 3.7302915560886126e-13; ///< time = l*sqrt(m/E)
+   const scalar pressure = 8.4305832688784826e8; ///< pressure = E/l**3
+   const scalar velocity = 4.5572845297447225e2; ///< velocity = sqrt(E/m)
 }
 
 /// atomic units
