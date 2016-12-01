9d9041d6880094b8dbd498d62545bfb3377d32f8
Albert Solernou <a.solernou@leeds.ac.uk>
2016-12-01 11:15:25 +0000

    [ctforces] rotational forces included, documentation not updated, new class arr3_view.
    Former-commit-id: 8923f24ed5d1a9bd1283d4df18a82b9a257fb350
    Former-commit-id: 51a6f3bb68c1981e7f0638c65122808d529d5138

--- include/mat_vec_fns_II.h
+++ include/mat_vec_fns_II.h
@@ -22,45 +22,53 @@ int getMissingNode(int n0, int n1, int n2);
 
 /** Add vectors vecA and vecB into res. */
 /** res can also be vecA or vecB */
-template <class brr3> void arr3arr3Add(brr3 &vecA, brr3 &vecB, brr3 &res);
+// template <class brr3> void arr3arr3Add(brr3 &vecA, brr3 &vecB, brr3 &res);
+template <class t_scalar, class brr3> void arr3arr3Add(arr3_view<t_scalar,brr3> vecA, arr3_view<t_scalar,brr3> vecB, arr3_view<t_scalar,brr3> res);
 
 /** res = vecA - vecB */
 /** res can be either vecA or vecB */
 // void arr3arr3Substract(arr3 &vecA, arr3 &vecB, arr3 &res); 
-template <class brr3> void arr3arr3Substract(brr3 &vecA, brr3 &vecB, brr3 &res);
+// template <class brr3> void arr3arr3Substract(brr3 &vecA, brr3 &vecB, brr3 &res);
+template <class t_scalar, class brr3> void arr3arr3Substract(arr3_view<t_scalar,brr3> vecA, arr3_view<t_scalar,brr3> vecB, arr3_view<t_scalar,brr3> res);
 
 /** w = u x v */
 /** w must be different from u and v */
 // void arr3arr3VectorProduct(arr3 (&u), arr3 (&v), arr3 (&w));
-template <class brr3> void arr3arr3VectorProduct(brr3 (&u), brr3 (&v), brr3 (&w));
+// template <class brr3> void arr3arr3VectorProduct(brr3 (&u), brr3 (&v), brr3 (&w));
+template <class t_scalar, class brr3> void arr3arr3VectorProduct(arr3_view<t_scalar,brr3> u, arr3_view<t_scalar,brr3> v, arr3_view<t_scalar,brr3> w);
 
 /** return the dot product for arrays vecA and vecB */
 // scalar arr3arr3DotProduct(arr3 &vecA, arr3 &vecB);
-template <class t_scalar, class brr3> t_scalar arr3arr3DotProduct(brr3 &vecA, brr3 &vecB);
+template <class t_scalar, class brr3> t_scalar arr3arr3DotProduct(arr3_view<t_scalar,brr3> vecA, arr3_view<t_scalar,brr3> vecB);
 
 /** Normalise vector arr3 e */
-template <class t_scalar, class brr3> void arr3Normalise(brr3 &e);
+template <class t_scalar, class brr3> void arr3Normalise(arr3_view<t_scalar,brr3> e);
 
 /** Get the normalised vector of arr3 e into arr3 n */
 // void arr3Normalise2(arr3 &e, arr3 &n);
-template <class t_scalar, class brr3> void arr3Normalise2(brr3 &e, brr3 &n);
+template <class t_scalar, class brr3> void arr3Normalise2(arr3_view<t_scalar,brr3> e, arr3_view<t_scalar,brr3> n);
 
 /** Given a scalar f, resize vector u*/
-template <class t_scalar, class brr3> void arr3Resize(t_scalar f, brr3 &u);
+template <class t_scalar, class brr3> void arr3Resize(t_scalar f, arr3_view<t_scalar,brr3> u);
 
 /** Given a scalar f, resize vector u into vector v */
 // void arr3Resize2(scalar f, arr3 &u, arr3 &v);
-template <class t_scalar, class brr3> void arr3Resize2(t_scalar f, brr3 &u, brr3 &v);
+template <class t_scalar, class brr3> void arr3Resize2(t_scalar f, arr3_view<t_scalar,brr3> u, arr3_view<t_scalar,brr3> v);
 
-template <class brr3> void arr3Store(brr3 &u, brr3 &v);
+/** cp arr3 u into arr3 v */
+template <class t_scalar, class brr3> void arr3Store(arr3_view<t_scalar,brr3> u, arr3_view<t_scalar,brr3> v);
 
-template <class t_scalar, class brr3> t_scalar arr3arr3Distance(brr3 &vecA, brr3 &vecB);
+/** return the distance from vecA to vecB */
+template <class t_scalar, class brr3> t_scalar arr3arr3Distance(arr3_view<t_scalar,brr3> vecA, arr3_view<t_scalar,brr3> vecB);
 
 /** Return the length of a vector v */
-template <class t_scalar> t_scalar mag(t_scalar (&v)[3]);
+template <class t_scalar, class brr3> t_scalar mag(arr3_view<t_scalar,brr3> v);
 
 /** Initialise the input vector with (0, 0, 0) */
 template <class brr3> void arr3Initialise(brr3 &v);
+
+/** calculate the determinant of a 3x3 matrix given by rows a, b, c */
+template <class t_scalar, class brr3> t_scalar detByRows(arr3_view<t_scalar,brr3> a, arr3_view<t_scalar,brr3> b, arr3_view<t_scalar,brr3> c); 
 
 
 ///////////////// SECTION 2 ////////////////////
@@ -114,6 +122,23 @@ template <class t_scalar, class brr3> bool nodeInTet(brr3 &vec, brr3 (tet)[4]);
 // void linePlaneIntersectionPoint(arr3 &ip, arr3 &e1, arr3 &e2, arr3 &p1, arr3 &p2, arr3 &p3);
 template <class t_scalar, class brr3> void linePlaneIntersectionPoint(brr3 &ip, brr3 &e1, brr3 &e2, brr3 &p1, brr3 &p2, brr3 &p3);
 
+/** Return true and 
+ *         the intersection point of the line that passes through the points e1 and e2
+ *           and the plane defined by points p1, p2, p3 if this intersection actually occurs,
+ *         and false otherwise. 
+ *         \warning p1, p2, and p3 are assumed to be non-colinear.
+ */
+template <class t_scalar, class brr3> bool safeLinePlaneIntersectionPoint(brr3 &ip, brr3 &e1, brr3 &e2, brr3 &p1, brr3 &p2, brr3 &p3);
+
+/** Return true and 
+ *         the intersection point ip of the line that passes through the points e1 and e2
+ *           and face defined by points p1, p2, p3 if this intersection actually occurs,
+ *         and false otherwise. 
+ *         \warning p1, p2, and p3 are assumed to be non-colinear.
+ */
+template <class t_scalar,class brr3> bool lineFaceIntersectionPoint(brr3 (&ip), brr3 (&e1), brr3 (&e2), brr3 (&p1), brr3 (&p2), brr3 (&p3));
+
+
 /** Check whether point ip is  
   *    inside of the three half-planes formed by the triangle's edges p1, p2, p3.
   */
@@ -137,11 +162,25 @@ template <class brr3> void faceCentroid(brr3 &p1, brr3 &p2, brr3 &p3, brr3 &c);
 /** Chech whether 4 points are on the same plane */
 template <class t_scalar, class brr3> bool samePlane(brr3 &p1, brr3 &p2, brr3 &p3, brr3 &p4);
 
+/** Given a line defined by point p1 and vector p2p1, get the intersecting point p3,
+ *   in that line from a third point p0.
+ * Essentially implementing "Intersection of two lines in three-space",
+ *  by Ronald Goldman, in Graphics Gems I. */
+template <class t_scalar, class brr3> void intersectingPointToLine(arr3_view<t_scalar,brr3> p0, arr3_view<t_scalar,brr3> p1, arr3_view<t_scalar,brr3> p2p1, arr3_view<t_scalar,brr3> p3);
+template <class t_scalar, class brr3> void intersectingPointToLine(vector3 &p0, arr3_view<t_scalar,brr3> p1, arr3_view<t_scalar,brr3> p2p1, arr3_view<t_scalar,brr3> p3);
+// template <class t_scalar, class brr3> void intersectingPointToLine(brr3 &p0, brr3 &p1, brr3 &p2, brr3 &p3);
+
+/** Given a line defined by points p1 and p2, 
+ *    return the distance from p0, to this line. */
+template <class t_scalar, class brr3> t_scalar distanceFromPointToLine(arr3_view<t_scalar,brr3> p0, arr3_view<t_scalar,brr3> p1, arr3_view<t_scalar,brr3> p2);
+
 ///////////////// SECTION 3 ////////////////////
 //// Transition functions from vector3 to arr3 // 
 ////////////////////////////////////////////////
 void vec3Vec3SubsToArr3(vector3 &u, vector3 &v, arr3 (&w));
 void vec3Arr3SubsToArr3(vector3 &u, arr3 &v, arr3 &w);
+void arr3Vec3SubsToArr3(arr3 (&u), vector3 &v, arr3 (&w));
+
 
 void vec3Arr3AddToArr3(vector3 &u, arr3 (&v), arr3 (&w));
 void vec3Vec3AddToArr3(vector3 &u, vector3 &v, arr3 (&w));
--- include/mat_vec_types.h
+++ include/mat_vec_types.h
@@ -1,6 +1,7 @@
 #ifndef MAT_VEC_TYPES_H_INCLUDED
 #define MAT_VEC_TYPES_H_INCLUDED
 
+#include <cstddef>
 #include <limits>
 #include <cmath> 
 
@@ -45,6 +46,41 @@ typedef struct {
 /** arr3 will hopefully substitute vector3 one day */ 
 typedef scalar arr3[3]; 
 typedef geoscalar grr3[3]; 
+
+/*  // The following class will be a temporary replacement
+    //   for the current vector3 struct so that we have some
+    //   time to change from Whatever.x into Whatever[0] 
+    // Then vector3 will become arr3.
+    // Therefore, don't ever populate this class with methods!!
+class vector3 {
+public:
+    scalar data[3]; 
+    scalar& x = data[0]; 
+    scalar& y = data[0]; 
+    scalar& z = data[0]; 
+    scalar& operator [](std::size_t i) { return data[i]; }
+};*/
+
+/** arr3_view has the spirit of "span" or "array_view" */
+template <class t_scalar, class brr3> class arr3_view
+{
+public:
+    arr3_view(brr3 (&arr) ) : data(arr) {}
+    arr3_view(t_scalar* data, std::size_t size) : data(data) {
+       // if (size != 3) // simple check removed in benefit of performance.
+       //  throw std::runtime_error("arr3 - wrong size of data: " + std::to_string(size)); 
+    }
+
+    t_scalar* begin() { return data; }
+    t_scalar* end() { return data + 3; }
+    t_scalar& operator [](std::size_t i) { return data[i]; }
+    // and similar stuff as above for const versions
+
+private:
+    t_scalar* data;
+}; 
+
+
 
 /**
  * Defines a 12 vector (just for ease of use, clarity and compiler type checking)
