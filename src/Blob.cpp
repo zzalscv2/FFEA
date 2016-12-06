zzalscv2/FFEA

commit 88f4469522a821ce365b7298a9ee115a6dc402a7

Added a fatal error if springs / forces are included with no background noise

src/Blob.cpp
@@ -225,6 +225,13 @@ int Blob::init(const int blob_index, const int conformation_index, const char *n
         calculate_node_element_connectivity();
         printf("\t\tdone\n");
 
+	// Run a check on parameters that are dependent upon the solver type
+	if(params->calc_stokes == 0 && (params->calc_springs == 1 || params->calc_ctforces == 1)) {
+		if(linear_solver == FFEA_NOMASS_CG_SOLVER) {
+			FFEA_ERROR_MESSG("For Blob %d, Conformation %d:\n\tSprings / Constant forces cannot be used with the CG_nomass solver if calc_stokes == 0\n\tThis constitutes an inertial system that cannot be solved our matrix inversion algorithms.\n\tPlease set calc_stokes = 1 and re-run the system :)\n", blob_index, conformation_index)
+		}				
+	}
+
         // Create the chosen linear equation Solver for this Blob
         if (linear_solver == FFEA_DIRECT_SOLVER) {
             solver = new SparseSubstitutionSolver();
@@ -719,7 +726,7 @@ vector3 Blob::position(scalar x, scalar y, scalar z) {
     vector3 v;
     // scalar dx, dy, dz;
 
-    // Calculate centroid of [SURFACE] Blob mesh
+    // Calculate centroid of entire Blob mesh
 #ifdef FFEA_PARALLEL_WITHIN_BLOB
 #pragma omp parallel for default(shared) private(i) reduction(+:centroid_x,centroid_y,centroid_z)
 #endif
@@ -812,6 +819,24 @@ void Blob::get_centroid(vector3 *com) {
     com->z /= num_nodes;
 }
 
+// This one returns an array rather than arsing about with pointers
+vector3 Blob::calc_centroid() {
+
+   vector3 com;
+   com.x = 0.0;	
+   com.y = 0.0;
+   com.z = 0.0;
+   for (int n = 0; n < num_nodes; n++) {
+        com.x += node[n].pos.x;
+        com.y += node[n].pos.y;
+        com.z += node[n].pos.z;
+   }
+   com.x /= num_nodes;
+   com.y /= num_nodes;
+   com.z /= num_nodes;
+   return com;
+}
+
 vector3 ** Blob::get_actual_node_positions() {
 	return node_position;
 }
@@ -834,12 +859,23 @@ void Blob::set_node_positions(vector3 *node_pos) {
     	}
 }
 
-void Blob::set_rmsd_pos_0() {
+void Blob::set_pos_0() {
+
+    CoG_0.x = 0.0;
+    CoG_0.y = 0.0;
+    CoG_0.z = 0.0;
     for (int i = 0; i < num_nodes; i++) {
         node[i].pos_0.x = node[i].pos.x;
         node[i].pos_0.y = node[i].pos.y;
         node[i].pos_0.z = node[i].pos.z;
+	CoG_0.x += node[i].pos_0.x;
+	CoG_0.y += node[i].pos_0.y;
+	CoG_0.z += node[i].pos_0.z;
     }
+    CoG_0.x /= num_nodes;
+    CoG_0.y /= num_nodes;
+    CoG_0.z /= num_nodes;
+
 }
 
 void Blob::kinetically_set_faces(bool state) {
@@ -1157,9 +1193,9 @@ void Blob::make_measurements() {
         }
 
         /* Calculate RMSD value for this configuration */
-#ifdef FFEA_PARALLEL_WITHIN_BLOB
-#pragma omp parallel for default(none) reduction(+:brmsd, cogx, cogy, cogz) private(i, temp1, temp2, temp3)
-#endif
+//#ifdef FFEA_PARALLEL_WITHIN_BLOB
+//#pragma omp parallel for default(none) reduction(+:brmsd, cogx, cogy, cogz) private(i, temp1, temp2, temp3)
+//#endif
         for (i = 0; i < num_nodes; i++) {
 
 	    /*
@@ -1168,16 +1204,32 @@ void Blob::make_measurements() {
 	    cogx += node[i].pos.x;
 	    cogy += node[i].pos.y;
 	    cogz += node[i].pos.z;
-
-            temp1 = node[i].pos.x - node[i].pos_0.x;
-            temp2 = node[i].pos.y - node[i].pos_0.y;
-            temp3 = node[i].pos.z - node[i].pos_0.z;
-            brmsd += temp1 * temp1 + temp2 * temp2 + temp3*temp3;
+	    
         }
-	rmsd = sqrt(brmsd / num_nodes);
 	CoG.x = cogx / num_nodes;
 	CoG.y = cogy / num_nodes;
 	CoG.z = cogz / num_nodes;
+
+	// Remove translation and rotation (maybe make optional in future)
+	bool remTrans = false;
+	bool remRot = true;
+	
+	if(remTrans) {
+		for(i = 0; i < num_nodes; ++i) {
+		    temp1 = node[i].pos.x - node[i].pos_0.x + CoG_0.x - CoG.x;
+        	    temp2 = node[i].pos.y - node[i].pos_0.y + CoG_0.y - CoG.y;
+        	    temp3 = node[i].pos.z - node[i].pos_0.z + CoG_0.z - CoG.z;
+        	    brmsd += temp1 * temp1 + temp2 * temp2 + temp3*temp3;
+		}
+	} else {
+		for(i = 0; i < num_nodes; ++i) {
+		    temp1 = node[i].pos.x - node[i].pos_0.x;
+        	    temp2 = node[i].pos.y - node[i].pos_0.y;
+        	    temp3 = node[i].pos.z - node[i].pos_0.z;
+        	    brmsd += temp1 * temp1 + temp2 * temp2 + temp3*temp3;
+		}
+	}
+	rmsd = sqrt(brmsd / num_nodes);
 	L.x = lx;
 	L.y = ly;
 	L.z = lz;

include/Blob.h
@@ -145,8 +145,9 @@ public:
      * Calculates and returns the centroid of this Blob
      */
     void get_centroid(vector3 *com);
+    vector3 calc_centroid();
 
-    void set_rmsd_pos_0();
+    void set_pos_0();
     void kinetically_set_faces(bool state);
     /**
      * Writes a new node file for the case of an initially translated STATIC blob, so viewer doesn't read straight from node file
@@ -502,7 +503,7 @@ private:
 
     //@{
     /** Geometries */
-    vector3 CoM, CoG;
+    vector3 CoM, CoG, CoM_0, CoG_0;
     scalar rmsd;
     //@}
 

src/NoMassCGSolver.cpp
@@ -136,6 +136,7 @@ int NoMassCGSolver::solve(vector3* x) {
     V->build();
     V->calc_inverse_diagonal(preconditioner);
     //V->print_dense_to_file(x);
+    //exit(0);
     int i = 0;
     scalar delta_new, delta_old, pTq, alpha;
     delta_new = conjugate_gradient_residual_assume_x_zero(x);
@@ -150,9 +151,11 @@ int NoMassCGSolver::solve(vector3* x) {
 
         // Once convergence is achieved, return
         if (residual2() < epsilon2) {
+	    //cout << residual2() << " " << epsilon2 << endl;
+	    //exit(0);
             return FFEA_OK;
         }
-
+	//cout << residual2() << " " << epsilon2 << endl;
         delta_old = delta_new;
         delta_new = parallel_apply_preconditioner();
         vec3_scale_and_add(p, z, (delta_new / delta_old), num_nodes);

src/World.cpp
@@ -422,7 +422,12 @@ int World::init(string FFEA_script_filename, int frames_to_delete, int mode, boo
 			}
 		}
 	}
-
+        // Now everything has been moved into boxes etc, save all initial positions
+	for(i = 0; i < params.num_blobs; ++i) {
+		for(j = 0; j < params.num_conformations[i]; ++j) {
+			blob_array[i][j].set_pos_0();
+		}
+	}
 	// If not restarting a previous simulation, create new trajectory and measurement files. But only if full simulation is happening!
 	if(mode == 0) {
 		// In any case, open the output checkpoint file for writing
@@ -1089,8 +1094,10 @@ int World::get_smallest_time_constants() {
 	cout << endl << "Global Time Constant Details:" << endl << endl;
 	cout << "\t\tFastest Mode: Blob " << dt_min_bin << ", tau (" << dt_min_world_type << ") = " << dt_min_world * mesoDimensions::time << "s" << endl;
 	cout << "\t\tSlowest Mode: Blob " << dt_max_bin << ", tau (" << dt_max_world_type << ") = " << dt_max_world * mesoDimensions::time << "s" << endl << endl;
-	cout << "\t\tPlease make sure your simulation timestep is less than " << dt_min_world * mesoDimensions::time << "s, for a stable simulation." << endl;
-	cout << "\t\tTake note than the energies will become inaccurate before this, so check your energy equilibrates correctly. If unsure, set dt << " << dt_min_world * mesoDimensions::time << "s" << endl << endl;
+	if(dt_min_world_type == "inertial") {
+		cout << "\t\tPlease make sure your simulation timestep is less than " << dt_min_world * mesoDimensions::time << "s, for a stable simulation." << endl;
+		cout << "\t\tTake note than the energies will become inaccurate before this, so check your energy equilibrates correctly. If unsure, set dt << " << dt_min_world * mesoDimensions::time << "s" << endl << endl;
+	}	
 	cout << "\t\tFor dynamical convergence, your simulation must run for longer than " << dt_max_world * mesoDimensions::time << "s." << endl << endl;
 
 	cout << "\t\tFINAL NOTE - If, after taking into account the above time constants, your simulation still fails (due to element inversion) it is not due to numerical instability from the integration, ";
@@ -2328,6 +2335,7 @@ int World::read_and_build_system(vector<string> script_vector) {
 		// vector3 *cent = new vector3;
 		for(j = 0; j < params.num_conformations[i]; ++j) {
 			cout << "\tInitialising blob " << i << " conformation " << j << "..." << endl;
+
 			if (blob_array[i][j].init(i, j, nodes.at(j).c_str(), topology.at(j).c_str(), surface.at(j).c_str(), material.at(j).c_str(), stokes.at(j).c_str(), vdw.at(j).c_str(), pin.at(j).c_str(), binding.at(j).c_str(), beads.at(j).c_str(),
                        		scale,calc_compress, compress, solver, motion_state.at(j), &params, &pc_params, &lj_matrix, &binding_matrix, rng, num_threads) == FFEA_ERROR) {
                        		FFEA_error_text();
@@ -2351,10 +2359,17 @@ int World::read_and_build_system(vector<string> script_vector) {
 		        	if (centroid != NULL) {
 
 		            		// Rescale first
+					//cout << centroid[0] << " " << centroid[1] << " " << centroid[2] << endl; 
 		            		centroid[0] *= scale;
 		            		centroid[1] *= scale;
 		            		centroid[2] *= scale;
+					//vector3 lol = blob_array[i][j].calc_centroid();
+					//cout << lol.x << " " << lol.y << " " << lol.z << endl;
 		            		vector3 dv = blob_array[i][j].position(centroid[0], centroid[1], centroid[2]);
+					//lol = blob_array[i][j].calc_centroid();
+					//cout << lol.x << " " << lol.y << " " << lol.z << endl;
+					//exit(0);
+
 		                        // if Blob has a number of beads, transform them too:
 		                        if (blob_array[i][j].get_num_beads() > 0)
 		                          blob_array[i][j].position_beads(dv.x, dv.y, dv.z);
@@ -2387,7 +2402,7 @@ int World::read_and_build_system(vector<string> script_vector) {
 
 		        	// set the current node positions as pos_0 for this blob, so that all rmsd values
 		        	// are calculated relative to this conformation centred at this point in space.
-		        	blob_array[i][j].set_rmsd_pos_0();
+		        	//blob_array[i][j].set_pos_0();
 			}
 			cout << "\t...done!" << endl;
 		}

commit a3de5f957445e5f32bba821a365ae16f583c0e33

[Setting] fatal error introduced in 88f4469, slightly changed and turned into warning error.
Former-commit-id: de9a2ec382458f86a115b46144214d8ed9e0791a
Former-commit-id: c598e7627d690a921d764cefe918787b8c0e2d45

src/Blob.cpp
@@ -240,9 +240,9 @@ int Blob::init(const int blob_index, const int conformation_index, const char *n
         printf("\t\tdone\n");
 
 	// Run a check on parameters that are dependent upon the solver type
-	if(params->calc_stokes == 0 && (params->calc_springs == 1 || params->calc_ctforces == 1)) {
+	if((params->calc_stokes == 0 && params->calc_noise == 0) && (params->calc_springs == 1 || params->calc_ctforces == 1)) {
 		if(linear_solver == FFEA_NOMASS_CG_SOLVER) {
-			FFEA_ERROR_MESSG("For Blob %d, Conformation %d:\n\tSprings / Constant forces cannot be used with the CG_nomass solver if calc_stokes == 0\n\tThis constitutes an inertial system that cannot be solved our matrix inversion algorithms.\n\tPlease set calc_stokes = 1 and re-run the system :)\n", blob_index, conformation_index)
+			FFEA_CAUTION_MESSG("For Blob %d, Conformation %d:\n\tUsing Springs / Constant forces in conjuction with the CG_nomass solver without calc_stokes or calc_noise may not converge, specially if there are very few ctforces or springs.\n\tIf you encounter this problem, consider setting either calc_stokes or calc_noise (you may try a low temperature) re-run the system :)\n", blob_index, conformation_index)
 		}				
 	}
 