37ebcf6ae1c07528877fe8151d13ea4fe9b5a93f
Rob Welch <py12rw@leeds.ac.uk>
2016-12-09 12:39:28 +0000

    Removed accidentally committed stuff
    Former-commit-id: 64681ecd022af011595daf9ffef0f7d3bc9896e2
    Former-commit-id: 7fa5bbf0df4506bbf1955a777bae92eb0869d075

--- build/lib.linux-x86_64-2.7/ffeatools/FFEA_analysis/FFEA_traj_tools/FFEA_get_num_frames.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-from FFEA_trajectory import get_num_frames as gnf
-
-if len(sys.argv) != 2:
-	sys.exit("Usage: python " + os.path.basename(os.path.abspath(sys.argv[0])) + " [FFEA traj .ftj]")
-
-print gnf(sys.argv[1])
-
--- build/lib.linux-x86_64-2.7/ffeatools/FFEA_initialise/Surface_tools/surface_coarse_grainer/__init__.py
+++
@@ -1,1 +0,0 @@
-from surface_coarse_grainer_final import surface_coarse_grainer
