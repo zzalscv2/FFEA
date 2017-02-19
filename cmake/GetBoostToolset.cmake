1d700a31384f3b8c9cef8baaa86b11c3e0f815aa
Albert Solernou <a.solernou@leeds.ac.uk>
2017-02-19 11:46:59 +0000

    [CMake] does build a subset of Boost. Results to be the default option.

---
+++ cmake/GetBoostToolset.cmake
@@ -0,0 +1,78 @@
+
+function(get_boost_toolset BOOST_TOOLSET) 
+   set(BOOST_TOOLSET "UNKNOWN")
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+      if (APPLE)
+        set(BOOST_TOOLSET "darwin" PARENT_SCOPE)
+      else(APPLE)
+        set(BOOST_TOOLSET "gcc" PARENT_SCOPE)
+      endif(APPLE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
+      set(BOOST_TOOLSET "intel" PARENT_SCOPE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+      set(BOOST_TOOLSET "clang" PARENT_SCOPE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Embarcadero, Borland")
+      set(BOOST_TOOLSET "borland" PARENT_SCOPE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "HP")
+      set(BOOST_TOOLSET "hp_cxx" PARENT_SCOPE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+      set(BOOST_TOOLSET "msvc" PARENT_SCOPE)
+   endif()
+
+   if (${CMAKE_CXX_COMPILER_ID} STREQUAL "SunPro")
+      set(BOOST_TOOLSET "sun" PARENT_SCOPE)
+   endif()
+endfunction()
+
+# 1 - Get the toolset:
+get_boost_toolset(BOOST_TOOLSET)
+if (BOOST_TOOLSET STREQUAL "UNKNOWN")
+   message(FATAL_ERROR "I don't know how to build Boost... you'd better do that yourself, and reconfigure with USE_BOOST_EXTERNAL")
+endif (BOOST_TOOLSET STREQUAL "UNKNOWN")
+
+# 2 - Set compiler flags:
+set(BOOST_COMPILER_FLAGS ${USE_CXX11_COMPILER_FLAGS})
+
+# 3 - Set some folders:
+set(BOOST_SOURCES "${PROJECT_SOURCE_DIR}/external/boost")
+set(BOOST_BUILD_DIR "${PROJECT_BINARY_DIR}/external/build_boost")
+set(BOOST_INSTALL_DIR "${PROJECT_BINARY_DIR}/external/install_boost")
+
+# 4 - Configure user-config.jam
+configure_file("${BOOST_SOURCES}/tools/build/user-config.jam.in"
+               "${BOOST_BUILD_DIR}/build_boost/user-config.jam" @ONLY)
+
+# --user-config="${PROJECT_BINARY_DIR}/build_boost/user-config.jam"
+# 5 - Configure, Build and Install: 
+include(ExternalProject)
+ExternalProject_Add(ourboost
+                    SOURCE_DIR ${BOOST_SOURCES}
+                    INSTALL_DIR ${BOOST_INSTALL_DIR}
+                    BUILD_IN_SOURCE 1
+                    BUILD_ALWAYS 0
+                    DEPENDERS ffea test_script_loader
+                    CONFIGURE_COMMAND ""
+                    BUILD_COMMAND ${BOOST_INSTALL_DIR}/bin/b2 link=static toolset=${BOOST_TOOLSET} --build-dir=${BOOST_BUILD_DIR} install --prefix=${BOOST_INSTALL_DIR} --user-config=${BOOST_BUILD_DIR}/build_boost/user-config.jam
+                    INSTALL_COMMAND ""
+                   )
+
+ExternalProject_Add_Step(ourboost unix_configure DEPENDERS configure
+                         WORKING_DIRECTORY ${BOOST_SOURCES}/tools/build
+                         COMMAND ./bootstrap.sh --with-toolset=${BOOST_TOOLSET}
+                         COMMAND ./b2 -j 4 toolset=${BOOST_TOOLSET} --build-dir=${BOOST_BUILD_DIR} install --prefix=${BOOST_INSTALL_DIR} --user-config=${BOOST_BUILD_DIR}/build_boost/user-config.jam)
+
+set(Boost_LIBRARY_DIRS ${BOOST_INSTALL_DIR}/lib)
+set(Boost_INCLUDE_DIRS ${BOOST_INSTALL_DIR}/include)
+set(Boost_LIBRARIES  ${BOOST_INSTALL_DIR}/lib/libboost_program_options.a;${BOOST_INSTALL_DIR}/lib/libboost_filesystem.a;${BOOST_INSTALL_DIR}/lib/libboost_system.a)
+
+
