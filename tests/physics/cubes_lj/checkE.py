9ccd596e2f8215d75da5ed259234062dfec295fa
Albert Solernou <a.solernou@leeds.ac.uk>
2017-05-26 17:55:16 +0100

    [VDW test] test files added for the final VdW test.

---
+++ tests/physics/cubes_lj/checkE.py
@@ -0,0 +1,28 @@
+# 
+#  This file is part of the FFEA simulation package
+#  
+#  Copyright (c) by the Theory and Development FFEA teams,
+#  as they appear in the README.md file. 
+# 
+#  FFEA is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  FFEA is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
+# 
+#  To help us fund FFEA development, we humbly ask that you cite 
+#  the research papers on the package.
+#
+
+# Doing the double surface integral  
+#      \int_S1 \int_S2 E = 1/r**12 - 2/r**6 dS1 dS2
+# numerically for 2 cubes of side 1 separated 1 gives -0.704384545,
+# the result obtained using CUBA (http://www.feynarts.de/cuba/)
+
