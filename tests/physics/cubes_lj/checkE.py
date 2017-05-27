33a5ff6c9eebde57b92216b2b5bbfffd0d8253dc
Albert Solernou <a.solernou@leeds.ac.uk>
2017-05-27 20:21:28 +0100

    [VdW test] testing the LJ energy calculation finished.
    Former-commit-id: 59efde8ef596a83d4d1fa5ea49dd411d4f24d298
    Former-commit-id: 530cfbfa50520d529ab5956a8440590acbeb6198

--- tests/physics/cubes_lj/checkE.py
+++ tests/physics/cubes_lj/checkE.py
@@ -21,8 +21,60 @@
 #  the research papers on the package.
 #
 
+import sys
+
+
+def readAndStore(iFile):
+  H = []
+  dH = {}
+  with open(iFile, 'r') as sta:
+    while (sta.readline() != "Measurements:\n"):
+      continue
+
+    blob = ""
+    readblob = False
+    for i in sta.readline().split():
+      if i == "|":
+        readblob = True
+        continue
+      if readblob:
+        blob = i
+        readblob = False
+        continue
+      H.append([blob+i])
+    for line in sta:
+      cnt = 0
+      if line.count("RESTART"): continue
+      for i in line.split():
+        H[cnt].append(float(i))
+        cnt += 1
+
+  for i in range(len(H)):
+    dH[H[i][0]] = i
+    H[i].pop(0)
+
+  return H, dH
+
+
 # Doing the double surface integral  
 #      \int_S1 \int_S2 E = 1/r**12 - 2/r**6 dS1 dS2
 # numerically for 2 cubes of side 1 separated 1 gives -0.704384545,
 # the result obtained using CUBA (http://www.feynarts.de/cuba/)
+# Thus, being the well depth and equilibrium distance values 
+#    in the .lj file: 2e537e17 J/m^4 and 1e-9m, the energy of the 
+#    first conformation is:
+knownValue = -1.7870e-19
+errTol = 1e-2
+fail = 0
 
+## ## ## STRAIGHT ## ## ## 
+iFile = ["veryFinefine1face1face.fm"]
+H, dH = readAndStore(iFile[0])
+
+errRel = abs((H[dH["VdWEnergy"]][-1] - knownValue)/knownValue) 
+if ( errRel > errTol ): 
+  print( "VdWEnergy energy should be %e, but was found to be %e" % (knownValue, (H[dH["VdWEnergy"]][-1])))
+  print( "The relative error: %e was larger than the tolerance: %e" % (errRel, errTol))
+  fail = 1
+
+sys.exit(fail)
